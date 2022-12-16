35dadd6e5f1fb800a33776fa3ecd81c55a997e7d
Lewis Dunn <lewis.dunn@playmint.com>
2022-12-16 16:33:12 +0000

    Local chain button in editor, shell scripts for it to invoke
    Commented some stuff out
    
    Removed some guff and set up easy and hard mode paths for osx
    
    Windows script
    
    Attempted windows batch script fix

---
+++ localChain.bat
@@ -0,0 +1,22 @@
+@echo off
+@echo %1
+
+if "%1%"=="easy" (
+	docker-compose down -v
+	docker-compose up
+)
+
+if "%1%"=="hard" (
+	cd ../ds-contracts
+	docker-compose down -v
+
+	cd ../cog-services
+	docker-compose down -v
+
+	cd ../ds-unity
+	docker-compose down -v
+
+	docker compose \
+		-f ../ds-contracts/docker-compose.yml \
+		up --build
+	)
