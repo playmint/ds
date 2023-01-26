b4a561acd8bc013b91d19c17bc7340837e47399d
JackPlaymint <jack.burfield@playmint.com>
2023-01-26 12:12:29 +0000

    Another fix for merge conflict

--- DawnSeekersUnity/Assets/Map/Scripts/GridExtensions.cs
+++ DawnSeekersUnity/Assets/Map/Scripts/GridExtensions.cs
@@ -7,14 +7,14 @@ public static class GridExtensions
     // Maths pilfered from https://www.redblobgames.com/grids/hexagons/
     
     // NOTE: Unity's grid uses Odd R offset coordinates
-    public static Vector3Int GridToCube(this Grid grid, Vector3Int gridCoords)
+    public static Vector3Int GridToCube(Vector3Int gridCoords)
     {
         int q = gridCoords.x - (gridCoords.y - (gridCoords.y & 1)) / 2;
         int r = gridCoords.y;
         return new Vector3Int(q, r, -q - r);
     }
 
-    public static Vector3Int CubeToGrid(this Grid grid, Vector3Int cubeCoords)
+    public static Vector3Int CubeToGrid(Vector3Int cubeCoords)
     {
         int x = cubeCoords.x + (cubeCoords.y - (cubeCoords.y & 1)) / 2;
         int y = cubeCoords.y;
