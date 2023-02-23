ae671e90e8a617a4d65a39152517a1c0f820c074
JackPlaymint <jack.burfield@playmint.com>
2023-02-23 12:53:29 +0000

    Added new zoom functionality, dragging is broken

--- DawnSeekersUnity/Assets/Map/Scripts/CameraController.cs
+++ DawnSeekersUnity/Assets/Map/Scripts/CameraController.cs
@@ -13,11 +13,16 @@ public class CameraController : MonoBehaviour
 
     public float transitionDuration = 1f;
     private Camera cam;
+    private float prevDirection = -100;
+    private Vector3 prevMousePosition;
+    private Vector3 mouseDownPos;
+    private Vector3 camMouseDownPos;
 
     void Start()
     {
         cam = GetComponent<Camera>();
         m_Plane = new Plane(Vector3.forward, 0);
+        Debug.Log("Version 1");
     }
 
     void Update()
@@ -29,42 +34,51 @@ public class CameraController : MonoBehaviour
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         float enter = 0.0f;
-
+        float speed = moveSpeed * Mathf.Abs(transform.position.z);
         if (m_Plane.Raycast(ray, out enter))
         {
-            Vector3 Scrolldirection = ray.GetPoint(enter);
+            Vector3 mousePosition = ray.GetPoint(enter);
 
-            float step = zoomSpeed * Time.deltaTime;
+            if (Input.GetMouseButtonDown(0))
+            {
+                mouseDownPos = mousePosition;
+                camMouseDownPos = transform.position;
+            }
+            Vector3 dragVector = Vector3.zero;
+            if (Input.GetMouseButton(0))
+            {
+                dragVector = (prevMousePosition - mousePosition);
+                transform.position = camMouseDownPos + dragVector;
+                Debug.Log(dragVector);
+            }
 
-            // Allows zooming in and out via the mouse wheel
-            if (Input.GetAxis("Mouse ScrollWheel") > 0 && enter > minZoom)
-            {
-                transform.position = Vector3.MoveTowards(
-                    transform.position,
-                    Scrolldirection,
-                    Mathf.Clamp(Input.GetAxis("Mouse ScrollWheel"), -1, 1) * step
-                );
-            }
-            if (Input.GetAxis("Mouse ScrollWheel") < 0 && enter < maxZoom)
-            {
-                transform.position = Vector3.MoveTowards(
-                    transform.position,
-                    Scrolldirection,
-                    Mathf.Clamp(Input.GetAxis("Mouse ScrollWheel"), -1, 1) * step
-                );
-            }
-            cam.orthographicSize = Mathf.Lerp(1, 5, Mathf.InverseLerp(minZoom, maxZoom, enter));
+            float z = Input.mouseScrollDelta.y;
+            float direction = 0;
+            if (z > 0)
+                direction = 1;
+            else if (z < 0)
+                direction = -1;
+
+
+
+            if (direction != prevDirection) {
+                Debug.Log("scroll wheel delta: " + z + ", has focus: " + Application.isFocused.ToString());
+                
+                prevDirection = direction;
+             }
+
+            float step = zoomSpeed * Time.deltaTime * prevDirection;
+            Vector3 inputVector = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0) * speed * Time.deltaTime;
+           
+            
+            
+            Vector3 movementOffset = inputVector;
+            Vector3 zoomOffset = (Vector3.Normalize(mousePosition - transform.position) * step);
+            transform.position += zoomOffset + movementOffset ;
+            if (transform.position.z > minZoom || transform.position.z < maxZoom)
+                transform.position -= zoomOffset;
+
+            prevMousePosition = mousePosition;
         }
-
-        //This is here because sometimes zooming glitches out for some reason:
-        if (transform.position.z >= 0)
-            transform.position -= Vector3.forward;
-        if (transform.position.z < -10)
-            transform.position = -Vector3.forward * 10;
-
-        transform.position +=
-            new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0)
-            * Time.deltaTime
-            * moveSpeed;
     }
 }
