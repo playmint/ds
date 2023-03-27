b7a227cc9aad92265c71b0a4e1c98e221489af8d
Jimmy Whitwell <jimmy.whitwell@playmint.com>
2023-03-27 19:15:46 +0100

    Action icons now driven by GameState again instead of internal state
    Removal of previous work around. This now fixes the problem of the action menu not disappearing when the seeker moves away from a selected tile

--- DawnSeekersUnity/Assets/Map/Scripts/UI/ActionMenuController.cs
+++ DawnSeekersUnity/Assets/Map/Scripts/UI/ActionMenuController.cs
@@ -9,63 +9,38 @@ public class ActionMenuController : MonoBehaviour
 {
     private void Start()
     {
-        // Cog.PluginController.Instance.EventStateUpdated += OnStateUpdated;
-
-        // NOTE: Decoupled from global UI because of a bug during movment where if the seeker
-        // Moves over a tile that is selected, it would see that the selected tile is at the
-        // Seeker's position and show the action menu again! Might have been able to get round this
-        // by checking the isMoving flag but for now simpler to just decouple from global state
-        MapInteractionManager.instance.EventTileLeftClick += OnTileLeftClick;
+        Cog.PluginController.Instance.EventStateUpdated += OnStateUpdated;
 
         gameObject.SetActive(false);
     }
 
     private void OnDestroy()
     {
-        // Cog.PluginController.Instance.EventStateUpdated += OnStateUpdated;
-
-        MapInteractionManager.instance.EventTileLeftClick -= OnTileLeftClick;
+        Cog.PluginController.Instance.EventStateUpdated -= OnStateUpdated;
     }
 
-    private void OnTileLeftClick(Vector3Int cellPosCube)
+    private void OnStateUpdated(State state)
     {
-        bool isPlayerAtPosition = SeekerManager.Instance.IsPlayerAtPosition(cellPosCube);
-        if (isPlayerAtPosition)
+        if (state.Selected.Tiles != null && state.Selected.Tiles.Count > 0)
         {
-            gameObject.SetActive(true);
-            transform.position = MapManager.instance.grid.CellToWorld(
-                GridExtensions.CubeToGrid(cellPosCube)
-            );
+            var tile = state.Selected.Tiles.ToList()[0];
+            var cellPosCube = TileHelper.GetTilePosCube(tile);
+            bool isPlayerAtPosition = SeekerManager.Instance.IsPlayerAtPosition(cellPosCube);
+            if (isPlayerAtPosition)
+            {
+                gameObject.SetActive(true);
+                transform.position = MapManager.instance.grid.CellToWorld(
+                    GridExtensions.CubeToGrid(cellPosCube)
+                );
+            }
+            else
+            {
+                gameObject.SetActive(false);
+            }
         }
         else
         {
             gameObject.SetActive(false);
         }
     }
-
-    // -- This code was driven by the global UI state which was causing some problems
-    // private void OnStateUpdated(State state)
-    // {
-    //     if (state.UI.Selection.Tiles != null && state.UI.Selection.Tiles.Count > 0)
-    //     {
-    //         var tile = state.UI.Selection.Tiles.ToList()[0];
-    //         var cellPosCube = TileHelper.GetTilePosCube(tile);
-    //         bool isPlayerAtPosition = SeekerManager.Instance.IsPlayerAtPosition(cellPosCube);
-    //         if (isPlayerAtPosition)
-    //         {
-    //             gameObject.SetActive(true);
-    //             transform.position = MapManager.instance.grid.CellToWorld(
-    //                 GridExtensions.CubeToGrid(cellPosCube)
-    //             );
-    //         }
-    //         else
-    //         {
-    //             gameObject.SetActive(false);
-    //         }
-    //     }
-    //     else
-    //     {
-    //         gameObject.SetActive(false);
-    //     }
-    // }
 }
