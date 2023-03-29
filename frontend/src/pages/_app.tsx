90da07e5fbbf54b14f93f8583816cbec1cad5a28
chrisfarms <chris@farmiloe.com>
2023-03-29 17:17:43 +0100

    frontend: remove redundent scout button
    scout plugin was temporary until map can scout again... be gone

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -7,15 +7,12 @@ import Head from 'next/head';
 import { GlobalStyles } from '@app/styles/global.styles';
 import { ModalProvider } from '@app/contexts/modal-provider';
 import { DSProvider } from '@dawnseekers/core';
-import scout from '../plugins/scout';
 import { InventoryProvider } from '@app/plugins/inventory/inventory-provider';
 
 const initialConfig = {
     wsEndpoint: process.env.NEXT_PUBLIC_DEFAULT_COG_WS_ENDPOINT || 'ws://localhost:8080/query',
     httpEndpoint: process.env.NEXT_PUBLIC_DEFAULT_COG_HTTP_ENDPOINT || 'http://localhost:8080/query'
 };
-
-const defaultPlugins = [scout];
 
 const App = ({ Component, pageProps }: AppProps) => {
     useEffect(() => {
@@ -37,7 +34,7 @@ const App = ({ Component, pageProps }: AppProps) => {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <GlobalStyles />
-            <DSProvider initialConfig={initialConfig} defaultPlugins={defaultPlugins}>
+            <DSProvider initialConfig={initialConfig}>
                 <InventoryProvider>
                     <ModalProvider>
                         <Component {...pageProps} />
