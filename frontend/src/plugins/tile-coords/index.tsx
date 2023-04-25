1f91202826d663ffd6adfbe560c7ba2a3eff51f5
Jimmy Whitwell <jimmy.whitwell@playmint.com>
2023-04-25 17:39:17 +0100

    Tile illustration hidden by default and show on coord click

--- frontend/src/plugins/tile-coords/index.tsx
+++ frontend/src/plugins/tile-coords/index.tsx
@@ -1,6 +1,6 @@
 /** @format */
 
-import { FunctionComponent } from 'react';
+import { FunctionComponent, useState } from 'react';
 import styled from 'styled-components';
 import { ComponentProps } from '@app/types/component-props';
 import { styles } from './tile-coords.styles';
@@ -19,20 +19,29 @@ export const TileCoords: FunctionComponent<TileCoordsProps> = (props: TileCoords
     const { selectedTiles, ...otherProps } = props;
     const lastTile = selectedTiles[selectedTiles.length - 1];
     const [_, q, r, s] = lastTile.coords.map((elm) => ethers.fromTwos(elm, 16));
+    const [showingIllustration, setShowingIllustration] = useState(false);
+    // const [__, qHex, rHex, sHex] = lastTile.coords;
+
+    const handleCoordinatesClick = () => {
+        setShowingIllustration(!showingIllustration);
+    };
 
     return (
         <StyledTileCoords {...otherProps}>
             <h3>Selected Tile Coordinates</h3>
-            <div className="tile-container">
-                <img src="empty-tile.png" alt="" width={80} />
-                <div className="element q">{`${q}`}</div>
-                <div className="element r">{`${r}`}</div>
-                <div className="element s">{`${s}`}</div>
-                <div className="axis q">{`Q`}</div>
-                <div className="axis r">{`R`}</div>
-                <div className="axis s">{`S`}</div>
-            </div>
-            <div className="coordinates">{`Q:${q} R:${r} S:${s}`}</div>
+            {showingIllustration && (
+                <div className="tile-container">
+                    <img src="empty-tile.png" alt="" width={80} />
+                    <div className="element q">{`${q}`}</div>
+                    <div className="element r">{`${r}`}</div>
+                    <div className="element s">{`${s}`}</div>
+                    <div className="axis q">{`Q`}</div>
+                    <div className="axis r">{`R`}</div>
+                    <div className="axis s">{`S`}</div>
+                </div>
+            )}
+            <div className="coordinates" onClick={handleCoordinatesClick}>{`${q}, ${r}, ${s}`}</div>
+            {/* <div className="coordinates">{`Q:${qHex} R:${rHex} S:${sHex}`}</div> */}
         </StyledTileCoords>
     );
 };
