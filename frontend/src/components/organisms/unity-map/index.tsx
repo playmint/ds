c7401a3e657d80e90fd765ac5ecd4a28f1e09191
JackPlaymint <jack.burfield@playmint.com>
2023-05-04 13:51:35 +0100

    Swapped out useCallback for useEffect

--- frontend/src/components/organisms/unity-map/index.tsx
+++ frontend/src/components/organisms/unity-map/index.tsx
@@ -77,26 +77,23 @@ export const UnityMap: FunctionComponent<UnityMapProps> = ({ ...otherProps }: Un
     // We'll use a state to store the device pixel ratio.
     const [devicePixelRatio, setDevicePixelRatio] = useState(window.devicePixelRatio);
 
-    const handleChangePixelRatio = useCallback(
-        function () {
-            // A function which will update the device pixel ratio of the Unity
-            // Application to match the device pixel ratio of the browser.
-            const updateDevicePixelRatio = function () {
-                setDevicePixelRatio(window.devicePixelRatio);
-            };
-            // A media matcher which watches for changes in the device pixel ratio.
-            const mediaMatcher = window.matchMedia(`screen and (resolution: ${devicePixelRatio}dppx)`);
-            // Adding an event listener to the media matcher which will update the
-            // device pixel ratio of the Unity Application when the device pixel
-            // ratio changes.
-            mediaMatcher.addEventListener('change', updateDevicePixelRatio);
-            return function () {
-                // Removing the event listener when the component unmounts.
-                mediaMatcher.removeEventListener('change', updateDevicePixelRatio);
-            };
-        },
-        [devicePixelRatio]
-    );
+    useEffect(() => {
+        // A function which will update the device pixel ratio of the Unity
+        // Application to match the device pixel ratio of the browser.
+        const updateDevicePixelRatio = function () {
+            setDevicePixelRatio(window.devicePixelRatio);
+        };
+        // A media matcher which watches for changes in the device pixel ratio.
+        const mediaMatcher = window.matchMedia(`screen and (resolution: ${devicePixelRatio}dppx)`);
+        // Adding an event listener to the media matcher which will update the
+        // device pixel ratio of the Unity Application when the device pixel
+        // ratio changes.
+        mediaMatcher.addEventListener('change', updateDevicePixelRatio);
+        return function () {
+            // Removing the event listener when the component unmounts.
+            mediaMatcher.removeEventListener('change', updateDevicePixelRatio);
+        };
+    }, [devicePixelRatio]);
 
     // -- State update
 
