fff375272186e8c240a28897d2cc3d11c2b761fd
chrisfarms <chris@farmiloe.com>
2023-06-27 15:47:00 +0100

    cast State to StateGraph to fix test

--- contracts/test/rules/NamingRule.t.sol
+++ contracts/test/rules/NamingRule.t.sol
@@ -3,24 +3,22 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 
-import {Game} from "cog/Game.sol";
 import {State, AnnotationKind} from "cog/State.sol";
+import {StateGraph} from "cog/StateGraph.sol";
 import {Dispatcher} from "cog/Dispatcher.sol";
 
 import {Game as Dawnseekers} from "@ds/Game.sol";
 import {Actions} from "@ds/actions/Actions.sol";
-import {Schema, Node, Rel, LocationKey, BiomeKind, DEFAULT_ZONE} from "@ds/schema/Schema.sol";
-import {ItemUtils} from "@ds/utils/ItemUtils.sol";
-import {BuildingKind} from "@ds/ext/BuildingKind.sol";
+import {Schema, Node} from "@ds/schema/Schema.sol";
 
 using Schema for State;
 
 contract NamingRuleTest is Test {
     event AnnotationSet(bytes24 id, AnnotationKind kind, string label, bytes32 ref, string data);
 
-    Game internal game;
+    Dawnseekers internal game;
     Dispatcher internal dispatcher;
-    State internal state;
+    StateGraph internal state;
 
     // accounts
     address aliceAccount;
@@ -40,7 +38,7 @@ contract NamingRuleTest is Test {
         dispatcher = game.getDispatcher();
 
         // fetch the State to play with
-        state = game.getState();
+        state = StateGraph(address(game.getState()));
     }
 
     function testNameOwnedEntity() public {
