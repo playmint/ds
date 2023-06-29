b53024dd16c6852ddc5d08247e232bbc5e632fcd
Chris Baker <chris@cbaker.co.uk>
2023-06-29 13:43:47 +0100

    Add different formats of favicon

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -30,7 +30,15 @@ const App = ({ Component, pageProps }: AppProps) => {
             <Head>
                 <title>Downstream</title>
                 <meta name="description" content="Downstream" />
-                <link rel="icon" href="/favicon.ico" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+                <link rel="manifest" href="/favicon/site.webmanifest" />
+                <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#0d131f" />
+                <link rel="shortcut icon" href="/favicon/favicon.ico" />
+                <meta name="msapplication-TileColor" content="#0d131f" />
+                <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
+                <meta name="theme-color" content="#ffffff" />
             </Head>
             <GlobalStyles />
             <DSProvider initialConfig={initialConfig}>
