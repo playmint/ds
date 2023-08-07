986a4bed694b4b2d0e5681627f3ce9ae6439c802
chrisfarms <chris@farmiloe.com>
2023-08-07 17:28:29 +0100

    simply fetcher of items/buildings/players to get started

--- cli/src/commands/get.ts
+++ cli/src/commands/get.ts
@@ -1,14 +1,34 @@
-import { NodeSelectors } from '@downstream/core';
-import { solidityPacked } from 'ethers';
+import { getCoords } from '@downstream/core';
 import { pipe, take, toPromise } from 'wonka';
 
-const GET_ITEMS = `query GetItems($gameID: ID!) {
+const NODE_FRAGMENT = `
+                id
+                kind
+                key
+                keys
+                name: annotation(name: "name") { value }
+                owner: node(match: { kinds: "Player", via: { rel: "Owner" } }) {
+                    id
+                    kind
+                    addr: key
+                }
+                buildingkind: node(match: { kinds: "BuildingKind", via: { rel: "Is" } }) {
+                    id
+                    name: annotation(name: "name") { value }
+                }
+                location: edges(match: { kinds: "Tile", via: { rel: "Location" } }) {
+                    key
+                    tile: node {
+                        id
+                        coords: keys
+                    }
+                }
+`;
+const GET_NODES_BY_KIND = `query GetNodes($gameID: ID!, $kinds: [String!]!) {
     game(id: $gameID) {
         state(simulated: true) {
-            items: nodes(match: { kinds: "Item" }) {
-                id
-                name: annotation(name: "name") { value }
-                owner: node(match: { kinds: "Player", via: { rel: "Owner" } }) { id }
+            nodes(match: { kinds: $kinds }) {
+                ${NODE_FRAGMENT}
             }
         }
     }
@@ -26,129 +46,112 @@ const getItemDetails = (itemId) => {
         }, [] as string[])
         .map((n: string) => Number(BigInt(n)))
         .slice(-4);
-    return { stackable, green, blue, red };
+    return { stackable: stackable === 1, green, blue, red };
 };
 
-const getItems = async (ctx) => {
+const formatNodeObject = (node) => {
+    const name = node.name?.value || null;
+    const owner = node.owner?.addr;
+    if (node.kind == 'Item') {
+        const details = getItemDetails(node.id);
+        const { id, kind } = node;
+        return { kind, id, name, owner, ...details };
+    } else if (node.kind == 'Building') {
+        const buildingkind = node.buildingkind?.name?.value || '';
+        const { kind, id } = node;
+        const coords = node.location
+            .map((l) => getCoords(l.tile))
+            .map(({ q, r, s }) => [q, r, s])
+            .find(() => true);
+        return { kind, id, owner, buildingkind, coords };
+    } else if (node.kind == 'MobileUnit') {
+        const { kind, id } = node;
+        const coords = node.location
+            .map((l) => getCoords(l.tile))
+            .map(({ q, r, s }) => [q, r, s])
+            .find(() => true);
+        return { kind, id, owner, name, coords };
+    } else if (node.kind == 'BuildingKind') {
+        const { kind, id } = node;
+        return { kind, id, name, owner };
+    } else if (node.kind == 'Player') {
+        const { kind, id, key } = node;
+        return { kind, id, addr: key, name };
+    } else if (node.kind == 'Tile') {
+        const { q, r, s } = getCoords({ coords: node.keys });
+        const coords = [q, r, s];
+        const { kind, id } = node;
+        return { kind, id, coords };
+    } else {
+        throw new Error(`formatting for ${node.kind} not implemented yet sorry`);
+    }
+};
+
+const getNodesByKind = async (ctx, kinds: string[]) => {
     const client = await ctx.client();
-    const res: any = await pipe(client.query(GET_ITEMS, { gameID: ctx.game }), take(1), toPromise);
-    return res.game.state.items.map(({ id, name, owner }) => {
-        const details = getItemDetails(id);
-        return { id, name: name?.value, owner: owner?.id, ...details };
-    });
+    const res: any = await pipe(client.query(GET_NODES_BY_KIND, { gameID: ctx.game, kinds }), take(1), toPromise);
+    return res.game.state.nodes.map(formatNodeObject);
 };
 
-const register = {
-    command: 'register <id>',
-    describe: 'register a custom item kind',
-    builder: (yargs) =>
-        yargs
-            .positional('id', {
-                describe: 'your extension id',
-                type: 'number',
-            })
-            .option('name', {
-                describe: 'name of building kind',
-                type: 'string',
-            })
-            .option('red', {
-                describe: 'amount of red goo in item',
-                type: 'number',
-            })
-            .option('green', {
-                describe: 'amount of green goo in item',
-                type: 'number',
-            })
-            .option('blue', {
-                describe: 'amount of blue goo in item',
-                type: 'number',
-            })
-            .option('icon', {
-                describe: 'an icon name to show in clients',
-                type: 'string',
-            })
-            .option('stackable', {
-                describe: 'is the item stackable',
-                type: 'boolean',
-            })
-            .demand(['id', 'name', 'red', 'green', 'blue'])
-            .check((argv) => {
-                if (typeof argv.id !== 'undefined' && argv.id < 1) {
-                    throw new Error(`id must be greater than 1`);
-                }
-                return true;
-            }),
-    handler: async (ctx) => {
-        const player = await ctx.player();
+const kindNames = {
+    Item: ['items', 'item'],
+    Player: ['players', 'player'],
+    BuildingKind: ['buildingkinds', 'buildingkind'],
+    Building: ['buildings', 'building'],
+    Tile: ['tiles', 'tile'],
+    MobileUnit: ['mobileunit', 'mobileunits', 'unit', 'units'],
+};
 
-        // build item id
-        // uint32[3] memory outputItemAtoms = [uint32(cfg.greenGoo), uint32(cfg.blueGoo), uint32(cfg.redGoo)];
-        // bytes24 itemKind = Node.Item(uint32(cfg.id), outputItemAtoms, cfg.stackable);
-        const itemID = solidityPacked(
-            ['bytes4', 'uint32', 'uint32', 'uint32', 'uint32', 'uint32'],
-            [NodeSelectors.Item, ctx.id, ctx.stackable ? 1 : 0, ctx.green, ctx.blue, ctx.red]
-        );
-        console.log('item', itemID);
+const normalizeKind = (givenKindName) => {
+    return Object.keys(kindNames).find((kindName) => kindNames[kindName].includes(givenKindName.toLowerCase()));
+};
 
-        await player.dispatch({
-            name: 'REGISTER_ITEM_KIND',
-            args: [itemID, ctx.name, ctx.icon],
+const command = {
+    command: 'get <kind> [id]',
+    describe: `Display one or many game entities. Prints a table of the most important information about the specified entities.`,
+    builder: (cli) => {
+        cli.positional('kind', {
+            describe: 'entity kind',
+            type: 'string',
+            choices: Object.values(kindNames).flatMap((v) => v),
         });
-
-        ctx.output.write('ok');
-        process.exit(0);
-    },
-};
-
-const list = {
-    command: 'list',
-    aliases: ['ls'],
-    describe: 'list items',
-    handler: async (ctx) => {
-        const items = await getItems(ctx);
-        ctx.output.write(items);
-    },
-};
-
-const search = {
-    command: 'search <pattern>',
-    describe: 'find items matching pattern',
-    builder: (cli) => {
-        cli.positional('pattern', { describe: 'substring to match name', type: 'string' });
-        cli.demand(['pattern']);
+        cli.positional('id', {
+            describe: 'filter by a specific id',
+            type: 'string',
+        });
+        cli.demand(['kind']);
+        cli.example([
+            ['$0 get items', 'Fetch all registered Items'],
+            ['$0 get item goo', 'Search for Items with "goo" in their name'],
+            ['$0 get item 0x4324...', 'Fetch a single entity by id'],
+            ['$0 get players', 'Fetch all registered players'],
+            ['$0 get buildingkinds', 'Fetch all registered building kinds'],
+        ]);
     },
     handler: async (ctx) => {
-        const re = new RegExp(ctx.pattern, 'i');
-        const items = (await getItems(ctx)).filter((item) => re.test(item.name || ''));
-        ctx.output.write(items);
+        const kind = ctx.kind
+            .split(',')
+            .map((k) => k.trim())
+            .map(normalizeKind)
+            .find((k) => !!k);
+        if (!kind) {
+            throw new Error(`must specify at least one kind or id`);
+        }
+
+        let nodes = await getNodesByKind(ctx, kind);
+
+        // filter by id or name
+        if (ctx.id) {
+            if (ctx.id.startsWith('0x')) {
+                nodes = nodes.filter((n) => n.id === ctx.id);
+            } else {
+                const re = new RegExp(ctx.id, 'i');
+                nodes = nodes.filter((n) => n.name && n.name.length > 0 && re.test(n.name));
+            }
+        }
+
+        ctx.output.write(nodes);
     },
 };
 
-const show = {
-    command: 'show <id>',
-    describe: 'item details',
-    builder: (cli) => {
-        cli.positional('id', { describe: 'id of item', type: 'string' });
-        cli.demand(['id']);
-    },
-    handler: async (ctx) => {
-        const items = await getItems(ctx);
-        const item = items.find((item) => item.id == ctx.id);
-        ctx.output.write(item);
-    },
-};
-
-export const item = {
-    command: 'items',
-    aliases: ['item'],
-    describe: 'list/show available items',
-    builder: (cli) => {
-        cli.command(list);
-        cli.command(show);
-        cli.command(search);
-        cli.command(register);
-        cli.demandCommand();
-    },
-};
-
-export default item;
+export default command;
