b57f8525abb7da5c13a0c0288f30549cbd1969b7
Paul Woodbridge <paul.woodbridge@playmint.com>
2023-09-08 13:38:36 +0100

    Hmmm...

--- contracts/test/rules/ExtractionRule.t.sol
+++ contracts/test/rules/ExtractionRule.t.sol
@@ -102,7 +102,7 @@ contract ExtractionRuleTest is Test, GameTest {
         state.setTileAtomValues(tile, [uint64(255), uint64(0), uint64(0)]);
 
         // -- Move time forward to completely fill reservoir
-        vm.roll(block.number + 1000);
+        vm.roll(block.number + 2000);
 
         // extract
         vm.startPrank(address(mockBuildingContract));
