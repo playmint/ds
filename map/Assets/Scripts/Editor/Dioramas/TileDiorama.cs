508ae021cf15dc8b8bb20cf47ca8235fc4ca0abe
chrisfarms <chris@farmiloe.com>
2023-09-13 10:56:33 +0100

    fix offset coords

--- map/Assets/Scripts/Editor/Dioramas/TileDiorama.cs
+++ map/Assets/Scripts/Editor/Dioramas/TileDiorama.cs
@@ -22,13 +22,16 @@ class TileDiorama : IDiorama
             new()
             {
 
-                { "Tile/1", new TileData() { _instanceId = "Tile/1", id = "0x1", q = 0, r = 0, s = 0, height = 1 } },
-                { "Tile/2", new TileData() { _instanceId = "Tile/2", id = "0x2", q = 1, r = -1, s = 0, height = 1 } },
+                { "Tile/1", new TileData() { id = "0x1", q = 0, r = 0, s = 0, height = 1 } },
+                { "Tile/2", new TileData() { id = "0x2", q = 1, r = -1, s = 0, height = 1 } },
+                { "Tile/3", new TileData() { id = "0x3", q = -1, r = 1, s = 0, height = 1 } },
+                { "Tile/4", new TileData() { id = "0x4", q = 1, r = 0, s = -1, height = 1 } },
+                { "Tile/5", new TileData() { id = "0x5", q = -1, r = 0, s = 1, height = 1 } },
             },
             // everything that should be visible at step 1
             new()
             {
-                { "Tile/2", new TileData() { _instanceId = "Tile/2", id = "0x2", q = 1, r = -1, s = 0, height = 1 } },
+                { "Tile/2", new TileData() { id = "0x2", q = 1, r = -1, s = 0, height = 1 } },
             }
         };
     }
