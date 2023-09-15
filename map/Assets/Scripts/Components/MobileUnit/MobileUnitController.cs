771cb54cdf312dd47e18fa10bdd083802d05f4d3
Jimmy Whitwell <jimmy.whitwell@playmint.com>
2023-09-15 18:21:52 +0100

    Highlight and outline
    I've added movement onto a shared tile in the diorama however I'm not handling this yet

--- map/Assets/Scripts/Components/MobileUnit/MobileUnitController.cs
+++ map/Assets/Scripts/Components/MobileUnit/MobileUnitController.cs
@@ -5,6 +5,28 @@ using UnityEngine.EventSystems;
 
 public class MobileUnitController : BaseComponentController<MobileUnitData>
 {
+    [SerializeField]
+    protected Renderer[] outlineObjs;
+
+    [SerializeField]
+    protected Renderer[] renderers;
+
+    [SerializeField]
+    protected Color highlightColor;
+
+    public Material redOutlineMat,
+        greenOutlineMat;
+    protected Color _defaultColor;
+
+    protected void Start()
+    {
+        // _renderers = GetComponentsInChildren<Renderer>();
+        if (renderers.Length > 0)
+        {
+            _defaultColor = renderers[0].material.GetColor("_EmissionColor");
+        }
+    }
+
     protected void Update()
     {
         if (_prevData == _nextData)
@@ -31,6 +53,41 @@ public class MobileUnitController : BaseComponentController<MobileUnitData>
             Mathf.Lerp(_nextData.heightPrev, _nextData.heightNext, _nextData.progress),
             Mathf.Lerp(worldPosPrev.z, worldPosNext.z, _nextData.progress)
         );
+
+        // selected
+        if (_nextData.selected == "outline")
+        {
+            foreach (Renderer outlineObj in outlineObjs)
+            {
+                outlineObj.material = redOutlineMat;
+            }
+            foreach (Renderer rend in renderers)
+            {
+                rend.material.SetColor("_EmissionColor", _defaultColor);
+            }
+        }
+        else if (_nextData.selected == "highlight")
+        {
+            foreach (Renderer outlineObj in outlineObjs)
+            {
+                outlineObj.material = greenOutlineMat;
+            }
+            foreach (Renderer rend in renderers)
+            {
+                rend.material.SetColor("_EmissionColor", highlightColor);
+            }
+        }
+        else
+        {
+            foreach (Renderer outlineObj in outlineObjs)
+            {
+                outlineObj.material = greenOutlineMat;
+            }
+            foreach (Renderer rend in renderers)
+            {
+                rend.material.SetColor("_EmissionColor", _defaultColor);
+            }
+        }
 
         _prevData = _nextData;
     }
--- map/Assets/Scripts/Editor/Dioramas/MobileUnitDiorama.cs
+++ map/Assets/Scripts/Editor/Dioramas/MobileUnitDiorama.cs
@@ -15,12 +15,14 @@ class MobileUnitDiorama : IDiorama
             {
 
                 { "Tile/1", new TileData() { q = 0, r = 0, s = 0, height = 0.1f } },
-                { "Tile/2", new TileData() { q = -1, r = 1, s = 0, height = 0.2f } },
-                { "Tile/3", new TileData() { q = 0, r = 1, s = -1, height = 0.3f } },
-                { "Tile/4", new TileData() { q = 1, r = 0, s = -1, height = 0.2f } },
-                { "Tile/5", new TileData() { q = 1, r = -1, s = 0, height = 0.1f } },
-                { "Tile/6", new TileData() { q = 0, r = -1, s = 1, height = 0.2f } },
-                { "Tile/7", new TileData() { q = -1, r = 0, s = 1, height = 0.4f } },
+                { "Tile/2", new TileData() { q = 1, r = -1, s = 0, height = 0.1f } },
+                { "Tile/3", new TileData() { q = 0, r = -1, s = 1, height = 0.1f } },
+                { "Tile/4", new TileData() { q = -1, r = 0, s = 1, height = 0.4f } },
+
+                { "Tile/5", new TileData() { q = -2, r = 1, s = 1, height = 0.4f } },
+                { "Tile/6", new TileData() { q = -1, r = 1, s = 2, height = 0.4f } },
+
+                // Movement
                 { "MobileUnit/1", new MobileUnitData() { 
                     qNext = -1,
                     rNext = 0,
@@ -34,16 +36,74 @@ class MobileUnitDiorama : IDiorama
                     selected = "none",
                     shared = false
                 } },
+
+                // Highlight / outline
+                { "MobileUnit/2", new MobileUnitData() { 
+                    qNext = 1,
+                    rNext = -1,
+                    sNext = 0,
+                    heightNext = 0.1f,
+                    qPrev = 0,
+                    rPrev = 0,
+                    sPrev = 0,
+                    heightPrev = 0.1f,
+                    progress = 1,
+                    selected = "highlight",
+                    shared = false
+                } },
+                { "MobileUnit/3", new MobileUnitData() { 
+                    qNext = 0,
+                    rNext = -1,
+                    sNext = 1,
+                    heightNext = 0.1f,
+                    qPrev = 0,
+                    rPrev = 0,
+                    sPrev = 0,
+                    heightPrev = 0.1f,
+                    progress = 1,
+                    selected = "outline",
+                    shared = false
+                } },
+
+                // Movement to shared tile
+                { "MobileUnit/4", new MobileUnitData() { 
+                    qNext = -2,
+                    rNext = 1,
+                    sNext = 1,
+                    heightNext = 0.4f,
+                    qPrev = 0,
+                    rPrev = 0,
+                    sPrev = 0,
+                    heightPrev = 0.1f,
+                    progress = 1,
+                    selected = "none",
+                    shared = false
+                } },
+                { "MobileUnit/5", new MobileUnitData() { 
+                    qNext = -1,
+                    rNext = 1,
+                    sNext = 2,
+                    heightNext = 0.4f,
+                    qPrev = 0,
+                    rPrev = 0,
+                    sPrev = 0,
+                    heightPrev = 0.1f,
+                    progress = 1,
+                    selected = "none",
+                    shared = false
+                } },
             },
             new()
             {
                 { "Tile/1", new TileData() { q = 0, r = 0, s = 0, height = 0.1f } },
-                { "Tile/2", new TileData() { q = -1, r = 1, s = 0, height = 0.2f } },
-                { "Tile/3", new TileData() { q = 0, r = 1, s = -1, height = 0.3f } },
-                { "Tile/4", new TileData() { q = 1, r = 0, s = -1, height = 0.2f } },
-                { "Tile/5", new TileData() { q = 1, r = -1, s = 0, height = 0.1f } },
-                { "Tile/6", new TileData() { q = 0, r = -1, s = 1, height = 0.2f } },
-                { "Tile/7", new TileData() { q = -1, r = 0, s = 1, height = 0.4f } },
+                { "Tile/2", new TileData() { q = 1, r = -1, s = 0, height = 0.1f } },
+                { "Tile/3", new TileData() { q = 0, r = -1, s = 1, height = 0.1f } },
+                { "Tile/4", new TileData() { q = -1, r = 0, s = 1, height = 0.4f } },
+
+                { "Tile/5", new TileData() { q = -2, r = 1, s = 1, height = 0.4f } },
+                { "Tile/6", new TileData() { q = -1, r = 1, s = 2, height = 0.4f } },
+
+                // movement
                 { "MobileUnit/1", new MobileUnitData() { 
                     qNext = -1,
                     rNext = 0,
@@ -56,6 +116,62 @@ class MobileUnitDiorama : IDiorama
                     progress = 1,
                     selected = "none",
                     shared = false
+                } },
+
+                // highlight and outline
+                { "MobileUnit/2", new MobileUnitData() { 
+                    qNext = 1,
+                    rNext = -1,
+                    sNext = 0,
+                    heightNext = 0.1f,
+                    qPrev = 0,
+                    rPrev = 0,
+                    sPrev = 0,
+                    heightPrev = 0.1f,
+                    progress = 1,
+                    selected = "highlight",
+                    shared = false
+                } },
+                { "MobileUnit/3", new MobileUnitData() { 
+                    qNext = 0,
+                    rNext = -1,
+                    sNext = 1,
+                    heightNext = 0.1f,
+                    qPrev = 0,
+                    rPrev = 0,
+                    sPrev = 0,
+                    heightPrev = 0.1f,
+                    progress = 1,
+                    selected = "outline",
+                    shared = false
+                } },
+
+                // Movement to shared tile
+                { "MobileUnit/4", new MobileUnitData() { 
+                    qNext = -2,
+                    rNext = 1,
+                    sNext = 1,
+                    heightNext = 0.4f,
+                    qPrev = 0,
+                    rPrev = 0,
+                    sPrev = 0,
+                    heightPrev = 0.1f,
+                    progress = 1,
+                    selected = "none",
+                    shared = true
+                } },
+                { "MobileUnit/5", new MobileUnitData() { 
+                    qNext = -2,
+                    rNext = 1,
+                    sNext = 1,
+                    heightNext = 0.4f,
+                    qPrev = 0,
+                    rPrev = 0,
+                    sPrev = 0,
+                    heightPrev = 0.1f,
+                    progress = 1,
+                    selected = "none",
+                    shared = true
                 } },
             },
         };
