5c7a520ad91c78062f72c445f7943800dc05b678
Jimmy Whitwell <jimmy.whitwell@playmint.com>
2023-09-18 12:09:40 +0100

    Mobile Unit fade and move animations
    Also removed prev and next from data.

--- map/Assets/Scripts/Editor/Dioramas/MobileUnitDiorama.cs
+++ map/Assets/Scripts/Editor/Dioramas/MobileUnitDiorama.cs
@@ -24,73 +24,58 @@ class MobileUnitDiorama : IDiorama
 
                 // Movement
                 { "MobileUnit/1", new MobileUnitData() { 
-                    qNext = -1,
-                    rNext = 0,
-                    sNext = 1,
-                    heightNext = 0.4f,
-                    qPrev = 0,
-                    rPrev = 0,
-                    sPrev = 0,
-                    heightPrev = 0.1f,
+                    q = 0,
+                    r = 0,
+                    s = 0,
+                    height = 0.1f,
                     progress = 0,
                     selected = "none",
-                    shared = false
+                    shared = false,
+                    visible = true
                 } },
 
                 // Highlight / outline
                 { "MobileUnit/2", new MobileUnitData() { 
-                    qNext = 1,
-                    rNext = -1,
-                    sNext = 0,
-                    heightNext = 0.1f,
-                    qPrev = 0,
-                    rPrev = 0,
-                    sPrev = 0,
-                    heightPrev = 0.1f,
+                    q = 1,
+                    r = -1,
+                    s = 0,
+                    height = 0.1f,
                     progress = 1,
                     selected = "highlight",
-                    shared = false
+                    shared = false,
+                    visible = true
                 } },
                 { "MobileUnit/3", new MobileUnitData() { 
-                    qNext = 0,
-                    rNext = -1,
-                    sNext = 1,
-                    heightNext = 0.1f,
-                    qPrev = 0,
-                    rPrev = 0,
-                    sPrev = 0,
-                    heightPrev = 0.1f,
+                    q = 0,
+                    r = -1,
+                    s = 1,
+                    height = 0.1f,
                     progress = 1,
                     selected = "outline",
-                    shared = false
+                    shared = false,
+                    visible = true
                 } },
 
                 // Movement to shared tile
                 { "MobileUnit/4", new MobileUnitData() { 
-                    qNext = -2,
-                    rNext = 1,
-                    sNext = 1,
-                    heightNext = 0.4f,
-                    qPrev = 0,
-                    rPrev = 0,
-                    sPrev = 0,
-                    heightPrev = 0.1f,
+                    q = -2,
+                    r = 1,
+                    s = 1,
+                    height = 0.4f,
                     progress = 1,
                     selected = "none",
-                    shared = false
+                    shared = false,
+                    visible = true
                 } },
                 { "MobileUnit/5", new MobileUnitData() { 
-                    qNext = -1,
-                    rNext = 1,
-                    sNext = 2,
-                    heightNext = 0.4f,
-                    qPrev = 0,
-                    rPrev = 0,
-                    sPrev = 0,
-                    heightPrev = 0.1f,
+                    q = -1,
+                    r = 1,
+                    s = 2,
+                    height = 0.4f,
                     progress = 1,
                     selected = "none",
-                    shared = false
+                    shared = false,
+                    visible = true
                 } },
             },
             new()
@@ -105,73 +90,58 @@ class MobileUnitDiorama : IDiorama
 
                 // movement
                 { "MobileUnit/1", new MobileUnitData() { 
-                    qNext = -1,
-                    rNext = 0,
-                    sNext = 1,
-                    heightNext = 0.4f,
-                    qPrev = 0,
-                    rPrev = 0,
-                    sPrev = 0,
-                    heightPrev = 0.1f,
+                    q = -1,
+                    r = 0,
+                    s = 1,
+                    height = 0.4f,
                     progress = 1,
                     selected = "none",
-                    shared = false
+                    shared = false,
+                    visible = true
                 } },
 
                 // highlight and outline
                 { "MobileUnit/2", new MobileUnitData() { 
-                    qNext = 1,
-                    rNext = -1,
-                    sNext = 0,
-                    heightNext = 0.1f,
-                    qPrev = 0,
-                    rPrev = 0,
-                    sPrev = 0,
-                    heightPrev = 0.1f,
+                    q = 1,
+                    r = -1,
+                    s = 0,
+                    height = 0.1f,
                     progress = 1,
                     selected = "highlight",
-                    shared = false
+                    shared = false,
+                    visible = true
                 } },
                 { "MobileUnit/3", new MobileUnitData() { 
-                    qNext = 0,
-                    rNext = -1,
-                    sNext = 1,
-                    heightNext = 0.1f,
-                    qPrev = 0,
-                    rPrev = 0,
-                    sPrev = 0,
-                    heightPrev = 0.1f,
+                    q = 0,
+                    r = -1,
+                    s = 1,
+                    height = 0.1f,
                     progress = 1,
                     selected = "outline",
-                    shared = false
+                    shared = false,
+                    visible = true
                 } },
 
                 // Movement to shared tile
                 { "MobileUnit/4", new MobileUnitData() { 
-                    qNext = -2,
-                    rNext = 1,
-                    sNext = 1,
-                    heightNext = 0.4f,
-                    qPrev = 0,
-                    rPrev = 0,
-                    sPrev = 0,
-                    heightPrev = 0.1f,
+                    q = -2,
+                    r = 1,
+                    s = 1,
+                    height = 0.4f,
                     progress = 1,
                     selected = "none",
-                    shared = true
+                    shared = true,
+                    visible = false
                 } },
                 { "MobileUnit/5", new MobileUnitData() { 
-                    qNext = -2,
-                    rNext = 1,
-                    sNext = 1,
-                    heightNext = 0.4f,
-                    qPrev = 0,
-                    rPrev = 0,
-                    sPrev = 0,
-                    heightPrev = 0.1f,
+                    q = -2,
+                    r = 1,
+                    s = 1,
+                    height = 0.4f,
                     progress = 1,
                     selected = "none",
-                    shared = true
+                    shared = true,
+                    visible = true
                 } },
             },
         };
