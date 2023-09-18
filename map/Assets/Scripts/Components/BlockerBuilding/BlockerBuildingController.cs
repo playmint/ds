6cadcd53909921c6480c473cd6ac9edb215b0c4d
JackPlaymint <jack.burfield@playmint.com>
2023-09-18 15:23:44 +0100

    Forgot the rotation on BlockerBuildingController

--- map/Assets/Scripts/Components/BlockerBuilding/BlockerBuildingController.cs
+++ map/Assets/Scripts/Components/BlockerBuilding/BlockerBuildingController.cs
@@ -29,6 +29,7 @@ public class BlockerBuildingController : BaseComponentController<BlockerBuilding
         Vector3Int cubeCoords = new Vector3Int(_nextData.q, _nextData.r, _nextData.s);
         Vector3 worldPos = CoordsHelper.CubeToWorld(cubeCoords);
         transform.position = new Vector3(worldPos.x, _nextData.height, worldPos.z);
+        transform.GetChild(0).localEulerAngles = new Vector3(0, float.Parse(_nextData.rotation), 0);
 
         if (_prevData == null)
         {
