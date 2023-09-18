2a86d7aef27aac447cf3f2bdcad7414ace8cb46a
Jimmy Whitwell <jimmy.whitwell@playmint.com>
2023-09-18 13:29:24 +0100

    TileGoo and Diorama
    Note that `color` is currently defined as `red`, `green` or `blue` as opposed to the hex value.

--- map/Assets/Scripts/Components/TileGoo/TileGooController.cs
+++ map/Assets/Scripts/Components/TileGoo/TileGooController.cs
@@ -5,6 +5,17 @@ using UnityEngine.EventSystems;
 
 public class TileGooController : BaseComponentController<TileGooData>
 {
+    [SerializeField]
+    private GameObject _gooGreen;
+
+    [SerializeField]
+    private GameObject _gooBlue;
+
+    [SerializeField]
+    private GameObject _gooRed;
+
+    private GameObject _gooInstance;
+
     protected void Update()
     {
         if (_prevData == _nextData)
@@ -15,6 +26,43 @@ public class TileGooController : BaseComponentController<TileGooData>
         Vector3 worldPos = CoordsHelper.CubeToWorld(cubeCoords);
         transform.position = new Vector3(worldPos.x, _nextData.height, worldPos.z);
         _prevData = _nextData;
-    }
 
+        // Display the correct colour
+        // TODO: Implement a generic white sprite that can be coloured
+        if (_gooInstance != null)
+        {
+            Destroy(_gooInstance);
+        }
+
+        switch (_nextData.color)
+        {
+            case "red":
+                _gooInstance = Instantiate(_gooRed, transform, false);
+                break;
+            case "green":
+                _gooInstance = Instantiate(_gooGreen, transform, false);
+                break;
+            case "blue":
+                _gooInstance = Instantiate(_gooBlue, transform, false);
+                break;
+            default:
+                // No goo for specified colour
+                return;
+        }
+
+        _gooInstance.transform.Rotate(0, Random.Range(0, 12) * 30.0f, 0);
+
+        // Display the correct size
+        var smallGoo = _gooInstance.transform.Find("small");
+        if (smallGoo != null)
+        {
+            smallGoo.gameObject.SetActive(_nextData.size == "small");
+        }
+
+        var bigGoo = _gooInstance.transform.Find("big");
+        if (bigGoo != null)
+        {
+            bigGoo.gameObject.SetActive(_nextData.size == "big");
+        }
+    }
 }
--- map/Assets/Scripts/Components/TileGoo/TileGooData.cs
+++ map/Assets/Scripts/Components/TileGoo/TileGooData.cs
@@ -4,6 +4,6 @@ public class TileGooData : BaseComponentData
     public int r;
     public int s;
     public float height;
-    public string? color; // hex
+    public string? color; // red/green/blue TODO: make hex
     public string? size; // small/big
 }
