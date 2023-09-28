44367c33da3c9b1b336b092db111a4d7f34f0763
Paul Woodbridge <paul.woodbridge@playmint.com>
2023-09-28 14:06:12 +0100

    Fix for Control Tower

--- contracts/src/fixtures/IntroOrientation/ControlTower.js
+++ contracts/src/fixtures/IntroOrientation/ControlTower.js
@@ -24,6 +24,10 @@ export default function update({ selected, world }) {
         engineerDistance = distance(selectedUnit.nextLocation.tile, selectedTile);
     }
 
+    const placeholder = () => {
+        ds.log('placeholder');
+        return;
+    }
 
     //Show this if there is no selected engineer OR the engineer is not adjacent to the building's tile
     if (!selectedUnit) {
@@ -61,10 +65,11 @@ export default function update({ selected, world }) {
     }
 
     //Need to hook this up
-    var hasQuest2 = false;
+    var hasQuest2Active = false;
+    var hasQuest2Completed = false;
 
     //If quest 2 isn't active or completed
-    if (!hasUserLicense && !hasQuest2) {
+    if (!hasQuest2Active && !hasQuest2Completed) {
         return {
             version: 1,
             components: [
@@ -75,8 +80,8 @@ export default function update({ selected, world }) {
                         {
                             id: 'default',
                             type: 'inline',
-                            html: 'This should show when the player hasn\'t done quest 2 yet.',
-                            buttons: [{ text: 'Register User', type: 'action', action: craft, disabled: true }]
+                            html: 'Appears before has collected quest 2.',
+                            buttons: [{ text: 'Register User', type: 'action', action: placeholder, disabled: false }]
                         }
                     ],
                 },
@@ -85,7 +90,7 @@ export default function update({ selected, world }) {
     }
 
     //Show this if quest 2 is active
-    if (!hasUserLicense && hasQuest2) {
+    if (hasQuest2Active) {
         return {
             version: 1,
             components: [
@@ -97,72 +102,8 @@ export default function update({ selected, world }) {
                             id: 'default',
                             type: 'inline',
                             html: 'Registration Error! Please collect valid User License',
-                            buttons: [{ text: 'Register User', type: 'action', action: craft, disabled: true }]
+                            buttons: [{ text: 'Register User', type: 'action', action: placeholder, disabled: true }]
                         }
-                    ],
-                },
-            ],
-        };
-    }
-
-
-    // fetch the expected inputs item kinds
-    const requiredInputs = selectedBuilding?.kind?.inputs || [];
-    const want0 = requiredInputs.find(inp => inp.key == 0);
-    const want1 = requiredInputs.find(inp => inp.key == 1);
-    const want2 = requiredInputs.find(inp => inp.key == 2);
-
-    // fetch what is currently in the input slots
-    const inputSlots = selectedBuilding?.bags.find(b => b.key == 0).bag?.slots || [];
-    const got0 = inputSlots?.find(slot => slot.key == 0);
-    const got1 = inputSlots?.find(slot => slot.key == 1);
-    const got2 = inputSlots?.find(slot => slot.key == 2);
-
-    // fetch our output item details
-    const expectedOutputs = selectedBuilding?.kind?.outputs || [];
-    const out0 = expectedOutputs?.find(slot => slot.key == 0);
-
-    // try to detect if the input slots contain enough stuff to craft
-    const canCraft = selectedUnit
-        && want0 && got0 && want0.balance == got0.balance
-        && want1 && got1 && want1.balance == got1.balance
-        && want2 && got2 && want2.balance == got2.balance;
-
-    const craft = () => {
-        if (!selectedUnit) {
-            ds.log('no selected engineer');
-            return;
-        }
-        if (!selectedBuilding) {
-            ds.log('no selected building');
-            return;
-        }
-
-        ds.dispatch(
-            {
-                name: 'BUILDING_USE',
-                args: [selectedBuilding.id, selectedUnit.id, []]
-            },
-        );
-
-        ds.log('The hermit cuts off his hand and gives it to you.');
-    };
-
-    //Show this if there's a rubber duck
-    if (hasRubberDuck) {
-        return {
-            version: 1,
-            components: [
-                {
-                    type: 'building',
-                    id: 'crazy-hermit',
-                    content: [
-                        {
-                            id: 'default',
-                            type: 'inline',
-                            html: 'The crazy hermit looks in wonder at your rubber duck. "GIVE GREEN GOO?" he asks. He holds his hand out as if to offer a trade.',
-                            buttons: [{ text: 'It\'s a deal!', type: 'action', action: craft, disabled: !canCraft }],
-                        },
                     ],
                 },
             ],
