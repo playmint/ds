56fc270ca633e5cdbabb6c7e8a15080775befb15
JackPlaymint <jack.burfield@playmint.com>
2023-10-05 09:30:59 +0100

    Added FPS counter for debugging

---
+++ map/Assets/Scripts/Utility/Fps.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections;
+
+public class Fps : MonoBehaviour
+{
+    private float count;
+
+    private IEnumerator Start()
+    {
+        GUI.depth = 2;
+        while (true)
+        {
+            count = 1f / Time.unscaledDeltaTime;
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(Screen.width-100, Screen.height - 100, 100, 25), "FPS: " + Mathf.Round(count));
+    }
+}
