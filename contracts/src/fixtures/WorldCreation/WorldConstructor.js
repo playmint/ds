bd1734106afbceb5cf29a1ca217a5934158d3db9
Paul Woodbridge <paul.woodbridge@playmint.com>
2023-10-10 11:02:35 +0100

    Switched World Constructor to a factory

--- contracts/src/fixtures/WorldCreation/WorldConstructor.js
+++ contracts/src/fixtures/WorldCreation/WorldConstructor.js
@@ -1,18 +1,9 @@
 import ds from 'downstream';
 
-let questStage = 0;
+export default function update({ selected, world }) {
 
-export default async function update({ selected, world }) {
+    var q6cIsActive = false;
 
-    //Function to test the distance between two tiles
-    function distance(a, b) {
-        return (
-            (Math.abs(Number(BigInt.asIntN(16, a.coords[1])) - Number(BigInt.asIntN(16, b.coords[1]))) +
-                Math.abs(Number(BigInt.asIntN(16, a.coords[2])) - Number(BigInt.asIntN(16, b.coords[2]))) +
-                Math.abs(Number(BigInt.asIntN(16, a.coords[3])) - Number(BigInt.asIntN(16, b.coords[3])))) /
-            2
-        );
-    }
 
     const { tiles, mobileUnit } = selected || {};
     const selectedTile = tiles && tiles.length === 1 ? tiles[0] : undefined;
@@ -20,161 +11,82 @@ export default async function update({ selected, world }) {
     const selectedUnit = mobileUnit;
 
 
-    //Not using this at the moment
-    var engineerDistance = 0;
-    if (selectedUnit) {
-        engineerDistance = distance(selectedUnit.nextLocation.tile, selectedTile);
-    }
+    // fetch the expected inputs item kinds
+    const requiredInputs = selectedBuilding?.kind?.inputs || [];
+    const want0 = requiredInputs.find(inp => inp.key == 0);
+    const want1 = requiredInputs.find(inp => inp.key == 1);
 
+    // fetch what is currently in the input slots
+    const inputSlots = selectedBuilding?.bags.find(b => b.key == 0).bag?.slots || [];
+    const got0 = inputSlots?.find(slot => slot.key == 0);
+    const got1 = inputSlots?.find(slot => slot.key == 1);
 
-    const placeholder = () => {
-        questStage++;
-    }
+    // fetch our output item details
+    const expectedOutputs = selectedBuilding?.kind?.outputs || [];
+    const out0 = expectedOutputs?.find(slot => slot.key == 0);
+    const out1 = expectedOutputs?.find(slot => slot.key == 1);
 
-    //Show this if there is no selected engineer OR the engineer is not adjacent to the building's tile
-    if (!selectedUnit) {
-        return {
-            version: 1,
-            components: [
-                {
-                    type: 'building',
-                    id: 'control-tower',
-                    content: [
-                        {
-                            id: 'default',
-                            type: 'inline',
-                            html: `Select your unit to interact with a building`
-                        }
-                    ]
-                },
-            ],
-        };
-    }
+    // try to detect if the input slots contain enough stuff to craft
+    const canCraft = selectedUnit
+        && want0 && got0 && want0.balance == got0.balance
+        && want1 && got1 && want1.balance == got1.balance;
 
-    /*
-    //Look for a user license in their bags
-    var hasUserLicense = false
-    for (var j = 0; j < selectedUnit.bags.length; j++) {
-        for (var i = 0; i < 4; i++) {
-            if (selectedUnit.bags[j].bag.slots[i]) {
-                var slot = selectedUnit.bags[j].bag.slots[i];
-
-                if (slot.item && slot.item.id === 'User License' && slot.balance >= 1) {
-                    hasUserLicense = true;
-                }
-            }
+    const craft = () => {
+        if (!selectedUnit) {
+            ds.log('no selected unit');
+            return;
         }
-    }
-    */
+        if (!selectedBuilding) {
+            ds.log('no selected building');
+            return;
+        }
 
+        ds.dispatch(
+            {
+                name: 'BUILDING_USE',
+                args: [selectedBuilding.id, selectedUnit.id, []]
+            },
+        );
 
-    //If quest 2 isn't active or completed
-    if (questStage === 0) {
+        ds.log('A prescient rubber item manufacturer.');
+    };
+
+    if (!q6cIsActive) {
         return {
             version: 1,
             components: [
                 {
                     type: 'building',
-                    id: 'control-tower',
+                    id: 'world-constructor',
                     content: [
                         {
                             id: 'default',
                             type: 'inline',
-                            html: 'Appears before has collected quest 2',
-                            buttons: [{ text: 'Register User', type: 'action', action: placeholder, disabled: false }]
-                        }
+                            html: 'A broken component is frustrating MORTON\'s work on the next creation',
+                            buttons: [{ text: 'Repair', type: 'action', action: craft, disabled: !canCraft }]
+                        },
                     ],
                 },
             ],
         };
     }
 
-    //Show this if quest 2 is active
-    else if (questStage === 1) {
-        return {
-            version: 1,
-            components: [
-                {
-                    type: 'building',
-                    id: 'control-tower',
-                    content: [
-                        {
-                            id: 'default',
-                            type: 'inline',
-                            html: 'Registration Error! Please collect valid User License',
-                            buttons: [{ text: 'Register User', type: 'action', action: placeholder, disabled: true },
-                                        { text: 'Advance Quest', type: 'action', action: placeholder, disabled: false }] //placeholder
-                        }
-                    ],
-                },
-            ],
-        };
-    }
-
-    //Show this if quest 3 is active
-    else if (questStage >= 2 && questStage <= 4) {
-        return {
-            version: 1,
-            components: [
-                {
-                    type: 'building',
-                    id: 'control-tower',
-                    content: [
-                        {
-                            id: 'default',
-                            type: 'inline',
-                            html: 'User identified as a total N00b. Please accept newbie quests and level up A.S.A.P.',
-                            buttons: [{ text: 'Accept Orientation Quest', type: 'action', action: placeholder, disabled: questStage > 2 },
-                                { text: 'Accept Creation Quest', type: 'action', action: placeholder, disabled: questStage > 3 },
-                                { text: 'Advance Quest', type: 'action', action: placeholder, disabled: questStage < 4 } //Placeholder
-                            ]
-                        }
-                    ],
-                },
-            ],
-        };
-    }
-
-    else if (questStage >= 5 && questStage <= 7) {
-        return {
-            version: 1,
-            components: [
-                {
-                    type: 'building',
-                    id: 'control-tower',
-                    content: [
-                        {
-                            id: 'default',
-                            type: 'inline',
-                            html: 'Simulation abnormalies have been detected. Please accept normalisation assignments.',
-                            buttons: [{ text: 'Accept Paperclip Maximiser Quest', type: 'action', action: placeholder, disabled: questStage > 5 },
-                                { text: 'Accept Corrupted User Quest', type: 'action', action: placeholder, disabled: questStage > 6 },
-                                { text: 'Advance Quest', type: 'action', action: placeholder, disabled: questStage < 7 } //Placeholder
-                            ]
-                        }
-                    ],
-                },
-            ],
-        };
-    }
-
-    else {
-        return {
-            version: 1,
-            components: [
-                {
-                    type: 'building',
-                    id: 'control-tower',
-                    content: [
-                        {
-                            id: 'default',
-                            type: 'inline',
-                            html: 'No quest are available at this time'
-                        }
-                    ],
-                },
-            ],
-        };
-    }
+    else
+    return {
+        version: 1,
+        components: [
+            {
+                type: 'building',
+                id: 'world-constructor',
+                content: [
+                    {
+                        id: 'default',
+                        type: 'inline',
+                        html: 'Thanks to your efforts the World Constructor is hard at work'
+                    },
+                ],
+            },
+        ],
+    };
 }
 
--- contracts/src/fixtures/WorldCreation/WorldConstructor.sol
+++ contracts/src/fixtures/WorldCreation/WorldConstructor.sol
@@ -6,8 +6,7 @@ import {Actions} from "@ds/actions/Actions.sol";
 import {BuildingKind} from "@ds/ext/BuildingKind.sol";
 
 contract WorldConstructor is BuildingKind {
-    function use(Game ds, bytes24 buildingInstance, bytes24, /*actor*/ bytes memory /*payload*/ ) public {
-        ds.getDispatcher().dispatch(abi.encodeCall(Actions.EXTRACT, (buildingInstance)));
+    function use(Game ds, bytes24 buildingInstance, bytes24, /*mobileUnit*/ bytes calldata /*payload*/ ) public {
+        ds.getDispatcher().dispatch(abi.encodeCall(Actions.CRAFT, (buildingInstance)));
     }
 }
-
