ac65088d3687252bfd7566b71b877232b62f31a3
JackPlaymint <jack.burfield@playmint.com>
2023-10-13 11:07:08 +0100

    Added ground plane to BuilderCamera diorama

--- map/Assets/Scripts/Editor/Dioramas/BuilderCameraDiorama.cs
+++ map/Assets/Scripts/Editor/Dioramas/BuilderCameraDiorama.cs
@@ -22,12 +22,27 @@ class BuilderCameraDiorama : IDiorama
                 { "Tile/6", new TileData() { q = 0, r = -1, s = 1, height = 0f, color = "#506A95"} },
                 { "Tile/7", new TileData() { q = -1, r = 0, s = 1, height = 0f, color = "#1980E0" } },
                 { "Factory/7", new FactoryBuildingData() { q = -1, r = 0, s = 1, height = 0.01f, model = "03-04", rotation = -30, color = "#2DAEE0", shadowColor = "#135198" } },
+                { "GroundPlane/1", new GroundPlaneData() { height = -0.4f, color = "#33486D" } },
+            },
+            new()
+            {
+
+                { "Tile/1", new TileData() { q = 0, r = 0, s = 0, height = 0.01f, color = "#506A95" } },
+                { "Tile/2", new TileData() { q = -1, r = 1, s = 0, height = 0.01f, color = "#506A95" } },
+                { "Tile/3", new TileData() { q = 0, r = 1, s = -1, height = 0f, color = "#506A95" } },
+                { "Tile/4", new TileData() { q = 1, r = 0, s = -1, height = 0f, color = "#506A95" } },
+                { "Tile/5", new TileData() { q = 1, r = -1, s = 0, height = 0f, color = "#506A95" } },
+                { "Tile/6", new TileData() { q = 0, r = -1, s = 1, height = 0f, color = "#506A95" } },
+                { "Tile/7", new TileData() { q = -1, r = 0, s = 1, height = 0f, color = "#1980E0" } },
+                { "Factory/7", new FactoryBuildingData() { q = -1, r = 0, s = 1, height = 0.01f, model = "03-04", rotation = -30, color = "#2DAEE0", shadowColor = "#135198" } },
+                { "GroundPlane/1", new GroundPlaneData() { height = -0.4f, color = "#33486D" } },
             },
             new()
             {
                 { "Tile/7", new TileData() { q = -1, r = 0, s = 1, height = 0f, color = "#1980E0" } },
                 { "BuilderCamera", new BuilderCameraData() { q = -1, r = 0, s = 1, height = 0.01f} },
                 { "Factory/7", new FactoryBuildingData() { q = -1, r = 0, s = 1, height = 0.01f, model = "03-04", rotation = -30, color = "#2DAEE0", shadowColor = "#135198" } },
+                { "GroundPlane/1", new GroundPlaneData() { height = -0.4f, color = "#33486D" } },
             },
         };
     }
