d32ad97f6e25ab8be6be23b1e0076cc54727b42a
Paul Woodbridge <paul.woodbridge@playmint.com>
2023-10-13 12:47:15 +0100

    Getting there

--- contracts/src/fixtures/IntroOrientation/ControlTower.js
+++ contracts/src/fixtures/IntroOrientation/ControlTower.js
@@ -62,19 +62,17 @@ export default async function update({ selected, player }) {
         if (!quests) return 0;
 
         const questRegError = findQuestByName(QUEST_2);
-        if (!questRegError) return 0;
-
-        if (questRegError.status === QUEST_ACCEPTED) return 1;
+        if (!questRegError && towerState < 1) return 0;
 
         const questReturn = findQuestByName(QUEST_3);
         if (!questReturn) return 1;
-        if (questReturn.status === QUEST_ACCEPTED) return 2;
+        if (questReturn.status === QUEST_ACCEPTED || towerState < 2) return 2;
 
         // show newb quests if not complete
-        if (!areAllQuestsCompleted([QUEST_4, QUEST_5])) return 2;
+        if (!areAllQuestsCompleted([QUEST_4, QUEST_5])) return 3;
 
         // show advanced quests if not complete
-        if (!areAllQuestsCompleted([QUEST_6, QUEST_7])) return 3;
+        if (!areAllQuestsCompleted([QUEST_6, QUEST_7])) return 4;
 
         // out of quests
         return -1;
@@ -102,70 +100,68 @@ export default async function update({ selected, player }) {
 
     const questStage = getQuestStage();
 
-    const failVerificationOnQuest1 = () => {
+    const failVerification = () => {
         ds.sendQuestMessage("failCredentials");
+        ds.log("MESSAGE SENT");
         towerState = 1;
+        ds.log("TOWER STATE UPDATED");
     }
 
     const verificationSuccess = () => {
         ds.sendQuestMessage("passCredentials");
+        ds.log("MESSAGE SENT");
         towerState = 2;
+        ds.log("TOWER STATE UPDATED");
     }
 
     //If quest 2 isn't active or completed
-    if (questStage < 2) {
-
-        if (towerState === 0) {
-
-            return {
-                version: 1,
-                components: [
-                    {
-                        type: "building",
-                        id: "control-tower",
-                        content: [
-                            {
-                                id: "default",
-                                type: "inline",
-                                html: "M.O.R.T.O.N. welcomes you to Hexwood, and yet is wondering who you are. Please verify your credentials.",
-                                buttons: [
-                                    {
-                                        text: "Verify Credentials",
-                                        type: "action",
-                                        action: failVerificationOnQuest1,
-                                        disabled: false,
-                                    },
-                                ],
-                            },
-                        ],
-                    },
-                ],
-            };
-        }
-        else {
-            return {
-                version: 1,
-                components: [
-                    {
-                        type: "building",
-                        id: "control-tower",
-                        content: [
-                            {
-                                id: "default",
-                                type: "inline",
-                                html: "User Credentials cannot be verified.<br>Please resolve this issue at the Registration Office...<br>...or remove yourself from the Simulation!"
-                            }
-                        ],
-                    },
-                ],
-            }
+    if (questStage === 0) {
+        return {
+            version: 1,
+            components: [
+                {
+                    type: "building",
+                    id: "control-tower",
+                    content: [
+                        {
+                            id: "default",
+                            type: "inline",
+                            html: "M.O.R.T.O.N. welcomes you to Hexwood, and yet is wondering who you are. Please verify your credentials.",
+                            buttons: [
+                                {
+                                    text: "Verify Credentials",
+                                    type: "action",
+                                    action: failVerification,
+                                    disabled: false,
+                                },
+                            ],
+                        },
+                    ],
+                },
+            ],
+        };
+    }
+    else if (questStage === 1) {
+        return {
+            version: 1,
+            components: [
+                {
+                    type: "building",
+                    id: "control-tower",
+                    content: [
+                        {
+                            id: "default",
+                            type: "inline",
+                            html: "User Credentials cannot be verified.<br>Please resolve this issue at the Registration Office...<br>...or remove yourself from the Simulation!"
+                        }
+                    ],
+                },
+            ],
         }
     }
+ 
     //If quest 2 isn't active or completed
-    else if (questStage >= 2) {
-
-        if (towerState < 2) {
-
+    else if (questStage === 2) {
             return {
                 version: 1,
                 components: [
@@ -190,53 +186,77 @@ export default async function update({ selected, player }) {
                     },
                 ],
             };
+    }
+
+    else if (questStage === 3) {
+
+        const orientationQuest = findQuestByName(QUEST_4);
+        const creationQuest = findQuestByName(QUEST_5);
+
+        var orientationButton = {
+            text: "Accept Orientation Quest",
+            type: "action",
+            action: () => {
+                acceptQuest(
+                    "0xadbb33ce000000000000000000000000c533c3b1b9d5856c",
+                );
+            }, // TODO: use name instead of ID
+            disabled: false
+        };
+
+        var creationButton = {
+            text: "Accept Creation Quest",
+            type: "action",
+            action: () => {
+                acceptQuest(
+                    "0xadbb33ce000000000000000000000000de3bb0a48fe15c39",
+                );
+            },
+            disabled: false
+        };
+
+
+        var htmlString = "";
+        var buttons;
+        if (!orientationQuest && !creationQuest) {
+            htmlString = "Two quests are available for your skill level: 'Total Newb'.<br>Please accept one to improve simulation competency";
+            buttons = [orientationButton, creationButton];
+        }
+        else if (!orientationQuest) {
+            htmlString = "Orientation quest is still available for your skill level.<br>Acceptance and completion will improve user ability within the simulation";
+            buttons = [orientationButton];
+        }
+        else if (!creationQuest) {
+            htmlString = "Creation quest is still available for your skill level.<br>Acceptance and completion will show user's ability to compose improvements to the simulation";
+            buttons = [creationButton];
         }
         else {
-
-            const orientationQuest = findQuestByName(QUEST_4);
-            const creationQuest = findQuestByName(QUEST_5);
-            return {
-                version: 1,
-                components: [
-                    {
-                        type: "building",
-                        id: "control-tower",
-                        content: [
-                            {
-                                id: "default",
-                                type: "inline",
-                                html: 'User identified as with status: "Newb". Please accept newbie quests and level up A.S.A.P.',
-                                buttons: [
-                                    {
-                                        text: "Accept Orientation Quest",
-                                        type: "action",
-                                        action: () => {
-                                            acceptQuest(
-                                                "0xadbb33ce000000000000000000000000c533c3b1b9d5856c",
-                                            );
-                                        }, // TODO: use name instead of ID
-                                        disabled: !!orientationQuest,
-                                    },
-                                    {
-                                        text: "Accept Creation Quest",
-                                        type: "action",
-                                        action: () => {
-                                            acceptQuest(
-                                                "0xadbb33ce000000000000000000000000de3bb0a48fe15c39",
-                                            );
-                                        },
-                                        disabled: !!creationQuest,
-                                    },
-                                ],
-                            },
-                        ],
-                    },
-                ],
-            };
+            html = "No quests are available at this time. Please return when both 'Orientation' and 'Creation' quests are completed";
         }
 
+
+
+        return {
+            version: 1,
+            components: [
+                {
+                    type: "building",
+                    id: "control-tower",
+                    content: [
+                        {
+                            id: "default",
+                            type: "inline",
+                            html: htmlString,
+                            buttons: buttons,                    
+                        },
+                    ],
+                },
+            ],
+        };
+    }
+
         
-    } else if (questStage === 3) {
+    else if (questStage === 4) {
         const paperclipQuest = findQuestByName(QUEST_6);
         const corruptQuest = findQuestByName(QUEST_7);
 
