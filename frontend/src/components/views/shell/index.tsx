3d6e7b040a76e784446992a4bc1e39fc5a084579
Jimmy Whitwell <jimmy.whitwell@playmint.com>
2023-10-13 17:57:06 +0100

    lint

--- frontend/src/components/views/shell/index.tsx
+++ frontend/src/components/views/shell/index.tsx
@@ -7,7 +7,6 @@ import { MobileUnits } from '@app/components/map/MobileUnit';
 import { Tiles } from '@app/components/map/Tile';
 import { TileGoos } from '@app/components/map/TileGoo';
 import { trackEvent } from '@app/components/organisms/analytics';
-import { Logs } from '@app/components/organisms/logs';
 import { Onboarding } from '@app/components/organisms/onboarding';
 import { ItemPluginPanel } from '@app/components/panels/item-plugin-panel';
 import { MobileUnitPanel } from '@app/components/panels/mobile-unit-panel';
