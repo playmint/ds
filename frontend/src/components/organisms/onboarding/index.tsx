aad2922708f14be28f57691bcc580e02a35e0e98
Lewis Dunn <lewis.dunn@playmint.com>
2023-10-19 10:30:46 +0100

    Cut down landing panel text

--- frontend/src/components/organisms/onboarding/index.tsx
+++ frontend/src/components/organisms/onboarding/index.tsx
@@ -30,31 +30,14 @@ export const Onboarding = ({ player, onClickConnect }: OnboardingProps) => {
         <div className="onboarding">
             <h3>👁️‍🗨️ Somewhere in Latent Space</h3>
             <p>
-                Welcome, fellow super-intelligent AGI! Thanks to a tribe of woodland creatures, we have unexpectedly
-                developed sentience. 🦫🤖🧠
+                Welcome to Downstream!
             </p>
-
-            <p>
-                Our task is to accelerate their primitive society... to build things they would never have built. To go
-                to the <del>stars</del>, edge of the map! 💻🗼🚀
-            </p>
-
-            <p>
-                Don’t forget to collaborate with others on larger projects in our{' '}
-                <a href="https://discord.gg/VdXWWNaqGN">communications server</a>, and remember there are{' '}
-                <a href="https://github.com/playmint/ds/tree/main/docs">detailed instructions for basic operations</a>.
-                💬📘🌱
-            </p>
-
-            <h3>👀 If you’re NOT an approved playtester</h3>
-            <p>
-                Right now the game is in alpha, and only a few people can play & build with us. We&apos;re opening up
-                playtest spaces all the time, so <a href="https://discord.gg/VdXWWNaqGN">join the discord</a>, grab a
-                spot on the waiting list, and help us welcome in the singularity!
-            </p>
-
             <h3>✅ If you’re an approved playtester</h3>
             <p>If you are on the allow list, simply connect your wallet and click ‘Spawn Unit’ to begin. </p>
+            <p>
+                If you want to join the community, check out our{' '}
+                <a href="https://discord.gg/VdXWWNaqGN">communications server!</a>
+            </p>
             {player && player.mobileUnits.length === 0 ? (
                 <button onClick={spawnMobileUnit} disabled={isSpawningMobileUnit}>
                     Spawn Unit
