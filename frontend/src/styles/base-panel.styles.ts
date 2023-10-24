83d1c6886ef7b1ddef3c96013178a4e6c0033483
Jimmy Whitwell <jimmy.whitwell@playmint.com>
2023-10-24 16:28:38 +0100

    Header styling applied to unit panel

--- frontend/src/styles/base-panel.styles.ts
+++ frontend/src/styles/base-panel.styles.ts
@@ -3,11 +3,12 @@ import { colorMap, colors } from './colors';
 
 export const BasePanelStyles = css`
     --panel-padding: 1.5rem;
+    --panel-border-radius: 1.2rem;
 
     position: relative;
     background: ${colorMap.primaryBackground};
     color: ${colorMap.primaryText};
-    border-radius: 1.2rem;
+    border-radius: var(--panel-border-radius);
     padding: var(--panel-padding);
     border: ${colorMap.primaryBorderColor} 3px solid;
 `;
@@ -16,10 +17,10 @@ export const StyledBasePanel = styled.div`
     ${BasePanelStyles}
 `;
 export const StyledHeaderPanel = styled(StyledBasePanel)`
-    overflow: hidden;
     flex-shrink: 0;
     padding: 0;
     > .header {
+        border-radius: var(--panel-border-radius) var(--panel-border-radius) 0 0;
         padding: var(--panel-padding);
         background: ${colorMap.secondaryBackground};
         color: ${colors.grey_3};
