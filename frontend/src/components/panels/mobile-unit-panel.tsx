1f6da825929bf001b6dee942516fe1bd4a02214e
Jimmy Whitwell <jimmy.whitwell@playmint.com>
2023-10-24 12:22:06 +0100

    Unit panel more like Jonny's design

--- frontend/src/components/panels/mobile-unit-panel.tsx
+++ frontend/src/components/panels/mobile-unit-panel.tsx
@@ -7,44 +7,99 @@ import { useCallback, useMemo } from 'react';
 import { BasePanelStyles } from '@app/styles/base-panel.styles';
 import { TextButton } from '@app/styles/button.styles';
 import styled from 'styled-components';
+import { colors } from '@app/styles/colors';
+
+const StyledMobileUnitIcon = styled.div`
+    --width: 6.4rem;
+    width: var(--width);
+    height: calc(var(--width) * 1.3);
+    background: ${colors.grey_5};
+
+    mask-image: url('/icons/selectedFilled.svg');
+    mask-size: 100%;
+    mask-position: center;
+    mask-repeat: no-repeat;
+
+    > .inner {
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -57%);
+        width: 89%;
+        height: 100%;
+        background: ${colors.orange_0};
+        mask-image: url('/icons/HexBadge_Icons.svg');
+        mask-size: 100%;
+        mask-position: center;
+        mask-repeat: no-repeat;
+
+        > .icon {
+            position: absolute;
+            left: 50%;
+            top: 50%;
+            transform: translate(-50%, -50%);
+            width: 50%;
+            height: 50%;
+            background: white;
+            mask-image: url('/icons/UnitIcon.svg');
+            mask-size: 100%;
+            mask-position: center;
+            mask-repeat: no-repeat;
+        }
+    }
+`;
+
+const MobileUnitIcon = ({ className, onClick }) => {
+    return (
+        <StyledMobileUnitIcon className={className} onClick={onClick}>
+            <div className="inner">
+                <div className="icon" />
+            </div>
+        </StyledMobileUnitIcon>
+    );
+};
 
 const StyledMobileUnitPanel = styled.div`
     ${BasePanelStyles}
     margin-top: 3.5rem;
+
+    .bags > div:last-child .slots {
+        margin-bottom: 0;
+    }
 `;
 
 const MobileUnitContainer = styled.div`
-    display: flex;
-    justify-content: flex-end;
-    align-items: center;
     overflow: visible;
     min-height: 5rem;
+    margin-top: 3rem;
 
     position: relative;
 
     user-select: none;
 
-    > .shield {
-        /* background: red; */
+    > .unitIcon {
         position: absolute;
-        left: -2rem;
-        top: -5.5rem;
-        width: 8rem;
+        cursor: pointer;
+        left: 0rem;
+        top: -8.2rem;
     }
 
-    > .controls {
-        display: flex;
-        flex-direction: row;
+    > .label {
+        font-size: 2.8rem;
+        display: block;
         width: 100%;
 
-        .label {
-            padding: 0 0.5rem;
-            text-transform: uppercase;
-            display: block;
-            width: 100%;
-            text-align: center;
-            overflow: hidden;
+        overflow: hidden;
+        font-weight: 800;
+
+        > .hashTag {
+            color: ${colors.orange_0};
         }
+    }
+
+    > .location {
+        margin-bottom: 0.5rem;
+        color: ${colors.grey_3};
     }
 `;
 
@@ -83,31 +138,31 @@ export const MobileUnitPanel = () => {
         sendMessage('MapCamera', 'FocusTile', JSON.stringify({ q, r, s }));
     }, [selectMobileUnit, playerUnits, sendMessage, mapReady]);
 
-    const selectNextMobileUnit = useCallback(
-        (n: number) => {
-            if (playerUnits.length === 0) {
-                return;
-            }
-            if (!selectMobileUnit) {
-                return;
-            }
-            if (!selectedMobileUnit) {
-                return;
-            }
-            if (playerUnits.length === 0) {
-                return;
-            }
-            const mobileUnitIndex = playerUnits.map((s) => s.id).indexOf(selectedMobileUnit.id);
-            const nextIndex =
-                mobileUnitIndex + n > playerUnits.length - 1
-                    ? 0
-                    : mobileUnitIndex + n < 0
-                    ? playerUnits.length - 1
-                    : mobileUnitIndex + n;
-            selectMobileUnit(playerUnits[nextIndex].id);
-        },
-        [playerUnits, selectMobileUnit, selectedMobileUnit]
-    );
+    // const selectNextMobileUnit = useCallback(
+    //     (n: number) => {
+    //         if (playerUnits.length === 0) {
+    //             return;
+    //         }
+    //         if (!selectMobileUnit) {
+    //             return;
+    //         }
+    //         if (!selectedMobileUnit) {
+    //             return;
+    //         }
+    //         if (playerUnits.length === 0) {
+    //             return;
+    //         }
+    //         const mobileUnitIndex = playerUnits.map((s) => s.id).indexOf(selectedMobileUnit.id);
+    //         const nextIndex =
+    //             mobileUnitIndex + n > playerUnits.length - 1
+    //                 ? 0
+    //                 : mobileUnitIndex + n < 0
+    //                 ? playerUnits.length - 1
+    //                 : mobileUnitIndex + n;
+    //         selectMobileUnit(playerUnits[nextIndex].id);
+    //     },
+    //     [playerUnits, selectMobileUnit, selectedMobileUnit]
+    // );
 
     const nameEntity = useCallback(
         (entityId: string | undefined) => {
@@ -142,23 +197,16 @@ export const MobileUnitPanel = () => {
                     <StyledMobileUnitPanel>
                         <div className="mobile-unit-actions">
                             <MobileUnitContainer>
-                                <img
-                                    src="/mobile-unit-yours.png"
-                                    className="shield"
-                                    alt=""
-                                    onClick={selectAndFocusMobileUnit}
-                                />
-                                <div className="controls">
-                                    <button className="icon-button" onClick={() => selectNextMobileUnit(-1)}>
-                                        <img src="/icons/prev.png" alt="Previous" />
-                                    </button>
-                                    <span className="label" onDoubleClick={() => nameEntity(selectedMobileUnit?.id)}>
-                                        {formatNameOrId(selectedMobileUnit, 'Unit ')}
-                                    </span>
-                                    <button className="icon-button" onClick={() => selectNextMobileUnit(+1)}>
-                                        <img src="/icons/next.png" alt="Next" />
-                                    </button>
-                                </div>
+                                <MobileUnitIcon className="unitIcon" onClick={selectAndFocusMobileUnit} />
+                                <span className="label" onDoubleClick={() => nameEntity(selectedMobileUnit?.id)}>
+                                    <span className="hashTag">#</span>
+                                    {formatNameOrId(selectedMobileUnit, 'unit')}
+                                </span>
+                                {selectedMobileUnit.nextLocation?.tile && (
+                                    <div className="location">
+                                        {getTileCoordsFromId(selectedMobileUnit.nextLocation.tile.id).join(' ')}
+                                    </div>
+                                )}
                             </MobileUnitContainer>
                             <MobileUnitInventory mobileUnit={selectedMobileUnit} bags={world?.bags || []} />
                         </div>
