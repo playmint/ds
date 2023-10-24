f2da4e0f20c32adc13960e83ab90c9c2a56e8586
Paul Woodbridge <paul.woodbridge@playmint.com>
2023-10-24 14:16:17 +0100

    Fixed up another selectedUnit/mobileUnit error

--- contracts/src/fixtures/CorruptedUser/CorruptedUser.js
+++ contracts/src/fixtures/CorruptedUser/CorruptedUser.js
@@ -1,7 +1,7 @@
 import ds from "downstream";
 
 export default function update({ selected, world }) {
-    const { tiles, selectedUnit } = selected || {};
+    const { tiles, mobileUnit } = selected || {};
     const selectedTile = tiles && tiles.length === 1 ? tiles[0] : undefined;
     const selectedBuilding = (world?.buildings || []).find(b => selectedTile && b.location?.tile?.id === selectedTile.id);
     const selectedBuildingBags = selectedBuilding
@@ -14,6 +14,7 @@ export default function update({ selected, world }) {
         selectedBuildingBags.find((bag) => bag.equipee.key === 0);
     const inputSlots = inputBag && inputBag.slots.sort((a, b) => a.key - b.key);
 
+    const selectedUnit = mobileUnit;
     const selectedUnitBags = selectedUnit
         ? (world?.bags || []).filter(
               (bag) => bag.equipee?.node.id === selectedUnit.id,
