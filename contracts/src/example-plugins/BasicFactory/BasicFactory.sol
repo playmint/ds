48bcd92189ccae4559599e1cd08bd1cb73b6669d
5p0rt5BEArD <iain.gilfeather@playmint.com>
2023-11-09 15:27:35 +0000

    Commented out full alternatives to BasicFactory.use

--- contracts/src/example-plugins/BasicFactory/BasicFactory.sol
+++ contracts/src/example-plugins/BasicFactory/BasicFactory.sol
@@ -10,20 +10,28 @@ import {BuildingKind} from "@ds/ext/BuildingKind.sol";
 using Schema for State;
 
 contract BasicFactory is BuildingKind {
-    function use(Game ds, bytes24 buildingInstance, bytes24 /*actor*/, bytes memory /*payload*/ ) public {
-        // uncomment to restrict building use to certain Units
-        // you will need to also uncomment the actor param
-        // these restrictions will not be reflected in the UI unless you make
-        // similar changes in BasicFactory.js
-        //State state = GetState(ds);
-        //CheckIsFriendlyUnit(state, actor, buildingInstance);
-
-        // uncomment to require carrying an idCard
-        // you can change idCardItemId to another item id
-        //CheckIsCarryingItem(state, actor, idCardItemId);
-
+    function use(Game ds, bytes24 buildingInstance, bytes24, /*actor*/ bytes memory /*payload*/ ) public {
         ds.getDispatcher().dispatch(abi.encodeCall(Actions.CRAFT, (buildingInstance)));
     }
+
+    // version of use that restricts crafting to building owner, author or allow list
+    // these restrictions will not be reflected in the UI unless you make
+    // similar changes in BasicFactory.js
+    /*function use(Game ds, bytes24 buildingInstance, bytes24 actor, bytes memory ) public {
+        State state = GetState(ds);
+        CheckIsFriendlyUnit(state, actor, buildingInstance);
+
+        ds.getDispatcher().dispatch(abi.encodeCall(Actions.CRAFT, (buildingInstance)));
+    }*/
+
+    // version of use that restricts crafting to units carrying a certain item
+    /*function use(Game ds, bytes24 buildingInstance, bytes24 actor, bytes memory ) public {
+        // require carrying an idCard
+        // you can change idCardItemId to another item id
+        CheckIsCarryingItem(state, actor, idCardItemId);
+    
+        ds.getDispatcher().dispatch(abi.encodeCall(Actions.CRAFT, (buildingInstance)));
+    }*/
 
     function GetState(Game ds) internal returns (State) {
         return ds.getState();
