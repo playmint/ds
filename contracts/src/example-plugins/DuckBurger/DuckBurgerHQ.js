831edd4dc5644155c13e3580524670ccf507742e
Steedie <Steediekid@gmail.com>
2024-01-12 16:51:26 +0000

    timer added and game state implemented
    The addition of the timer allows the front end to understand the state of the game, and update the html based on that.

--- contracts/src/example-plugins/DuckBurger/DuckBurgerHQ.js
+++ contracts/src/example-plugins/DuckBurger/DuckBurgerHQ.js
@@ -1,6 +1,114 @@
 import ds from 'downstream';
 
+const prizeFee = 2;
+const buildingPrizeBagSlot = 0;
+const buildingPrizeItemSlot = 0;
+const unitPrizeBagSlot = 0;
+const unitPrizeItemSlot = 0;
+
+function getHQData(buildingId) {
+    const description = buildingId?.kind?.description?.value || "";
+    let values = description.split(', ').map(Number);
+    return {
+        prizePool: values[0],
+        gameActive: values[1] === 1,
+        endBlock: values[2]
+    };
+}
+
+function formatTime(timeInMs) {
+    let seconds = Math.floor(timeInMs / 1000);
+    let minutes = Math.floor(seconds / 60);
+    let hours = Math.floor(minutes / 60);
+
+    seconds %= 60;
+    minutes %= 60;
+
+    // Pad each component to ensure two digits
+    let formattedHours = String(hours).padStart(2, '0');
+    let formattedMinutes = String(minutes).padStart(2, '0');
+    let formattedSeconds = String(seconds).padStart(2, '0');
+
+    return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
+}
+
 export default async function update(state) {
+
+    const join = () => {
+        const mobileUnit = getMobileUnit(state);
+
+        const payload = ds.encodeCall(
+            "function join()",
+            []
+        );
+        
+        const dummyBagIdIncaseToBagDoesNotExist = `0x${'00'.repeat(24)}`;
+
+        ds.dispatch(
+            {
+                name: 'TRANSFER_ITEM_MOBILE_UNIT',
+                args: [
+                    mobileUnit.id,
+                    [mobileUnit.id, selectedBuilding.id],
+                    [unitPrizeBagSlot, buildingPrizeBagSlot ],
+                    [unitPrizeItemSlot, buildingPrizeItemSlot],
+                    dummyBagIdIncaseToBagDoesNotExist,
+                    prizeFee,
+                    ],
+            },
+            {
+                name: 'BUILDING_USE',
+                args: [selectedBuilding.id, mobileUnit.id, payload],
+            }
+        );
+    }
+
+    const start = () => {
+        const mobileUnit = getMobileUnit(state);
+        const payload = ds.encodeCall(
+            "function start(uint24 duckBuildingID, uint24 burgerBuildingID)",
+            [0, 0]
+        );
+        
+        ds.dispatch(
+            {
+                name: 'BUILDING_USE',
+                args: [selectedBuilding.id, mobileUnit.id, payload],
+            }
+        );
+     }
+
+    const claim = () => {
+        const mobileUnit = getMobileUnit(state);
+
+        const payload = ds.encodeCall(
+            "function claim()",
+            []
+        );
+        
+        ds.dispatch(
+            {
+                name: 'BUILDING_USE',
+                args: [selectedBuilding.id, mobileUnit.id, payload],
+            }
+        );
+     }
+
+    const reset = () => {
+        const mobileUnit = getMobileUnit(state);
+        const payload = ds.encodeCall(
+            "function reset()",
+            []
+        );
+        
+        ds.dispatch(
+            {
+                name: 'BUILDING_USE',
+                args: [selectedBuilding.id, mobileUnit.id, payload],
+            }
+        );
+     }
+
     // uncomment this to browse the state object in browser console
     // this will be logged when selecting a unit and then selecting an instance of this building
     //logState(state);
@@ -11,6 +119,10 @@ export default async function update(state) {
     const selectedTile = getSelectedTile(state);
     const selectedBuilding = selectedTile && getBuildingOnTile(state, selectedTile);
     
+    const {prizePool, gameActive, endBlock} = getHQData(selectedBuilding);
+
+
+
     // get contract data
     // - initially from description
     // - then converted to use the new data
@@ -21,61 +133,61 @@ export default async function update(state) {
     // - GameOver : GameActive == true && endBlock >= currentBlock
 
     let buttonList = [];
-    let htmlBlock = '';
+    let htmlBlock = '<p>Ducks vs Burgers HQ</p></br>';
+
+    htmlBlock += `<p>payout for win: ${prizeFee * 2}</p>`;
+    htmlBlock += `<p>payout for draw: ${prizeFee}</p></br>`;
     // map data
 
     // switch (state)
     // case NotStared:
     //  enable join
     //  check unit has entrance fee
-    const canJoin = false; // hasEntranceFee();
-    buttonList.push({ text: 'Join Game', type: 'action', action: join, disabled: !canJoin });
+    const canJoin = !gameActive; // hasEntranceFee();
+
+    if (canJoin){
+        htmlBlock += `<p>player's joined: ${prizePool > 0 ? prizePool / prizeFee : 0}</p>`;
+    }
+
+    buttonList.push({ text: `Join Game (${prizeFee} Green Goo)`, type: 'action', action: join, disabled: !canJoin });
+
+    const canStart = !gameActive && prizePool >= prizeFee * 2;
+
+    buttonList.push({ text: 'Start', type: 'action', action: start, disabled: !canStart });
+    //htmlBlock += `<p>Joined Unit is ${team1Units}</p>`
     //  check for enough joiners and enable start
     //
     // case Running:
     //  show count
+
+    const nowBlock = state?.world?.block;
+    const blocksLeft = endBlock > nowBlock ? endBlock - nowBlock : 0;
+    const timeLeftMs = blocksLeft * 2 * 1000;
+
+    if (gameActive && blocksLeft > 0){
+        
+        htmlBlock += `<p>time remaining: ${formatTime(timeLeftMs)}</p>`;
+    }
     //
     // case GameOver:
     // enable claim (if on winning team)
     // enable reset
-
-    const join = () => {
-        if (canJoin)
-            return; 
-
-        const mobileUnit = getMobileUnit(state);
-
-        const payload = ds.encodeCall(
-            "function join(bytes24 unit)",
-            [mobileUnit.id]
-          );
-        ds.dispatch({
-            name: 'BUILDING_USE',
-            args: [selectedBuilding.id, mobileUnit.id, payload],
-        });
-    }
-
-    const start = () => { }
-
-    const claim = () => { }
-
-    const reset = () => { }
-
-    
-    
+    const canClaim = gameActive && blocksLeft == 0;
+    buttonList.push({ text: prizePool > 0 ? `Claim Reward` : 'Nothing to Claim', type: 'action', action: claim, disabled: !canClaim });
+    buttonList.push({ text: 'Reset', type: 'action', action: reset, disabled: false });
 
     return {
         version: 1,
         // map: []
         components: [
             {
-                id: 'basic-factory',
+                id: 'dbhq',
                 type: 'building',
                 content: [
                     {
                         id: 'default',
                         type: 'inline',
-                        html: '<p>Fill the input slots to enable crafing</p>',
+                        html: htmlBlock,
                         buttons: buttonList,
                     },
                 ],
