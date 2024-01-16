3cf3a4ae28c0234b812bda15e8579ded50f2233c
5p0rt5BEArD <iain.gilfeather@playmint.com>
2024-01-16 13:30:29 +0000

    Re-instate some goo for new spawned units
    No goo is too annoying for testing

--- contracts/src/rules/NewPlayerRule.sol
+++ contracts/src/rules/NewPlayerRule.sol
@@ -53,16 +53,16 @@ contract NewPlayerRule is Rule {
             state.setNextLocation(mobileUnit, locationTile, ctx.clock);
             // give the mobileUnit a couple of bags
             /*bytes24 bag0 =*/ _spawnBag(state, mobileUnit, ctx.sender, 0);
-            /*bytes24 bag1 =*/ _spawnBag(state, mobileUnit, ctx.sender, 1);
+            bytes24 bag1 = _spawnBag(state, mobileUnit, ctx.sender, 1);
 
             //give the mobileUnit the two starter items
             //state.setItemSlot(bag0, 0, ItemUtils.AcceptanceLetter(), 1);
             //state.setItemSlot(bag0, 1, ItemUtils.IDCard(), 1);
 
             //give the mobileUnit 100 of each of the base materials
-            //state.setItemSlot(bag1, 0, ItemUtils.GreenGoo(), 100);
-            //state.setItemSlot(bag1, 1, ItemUtils.BlueGoo(), 100);
-            //state.setItemSlot(bag1, 2, ItemUtils.RedGoo(), 100);
+            state.setItemSlot(bag1, 0, ItemUtils.GreenGoo(), 100);
+            state.setItemSlot(bag1, 1, ItemUtils.BlueGoo(), 100);
+            state.setItemSlot(bag1, 2, ItemUtils.RedGoo(), 100);
 
             // Accept the first quest in the chain
             //state.setQuestAccepted(Node.Quest("Report to Control"), Node.Player(ctx.sender), 0);
