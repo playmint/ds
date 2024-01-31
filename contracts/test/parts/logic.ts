2b36cdd31079cbdc050e875e7c175452c96efb0c
Jimmy Whitwell <jimmy.whitwell@playmint.com>
2024-01-31 14:26:42 +0000

    Fixed tests to use ValueFrom objects instead of numbers for index. Changed `pressed` variable to be int as uint will underflow

--- contracts/test/parts/logic.ts
+++ contracts/test/parts/logic.ts
@@ -1,115 +1,107 @@
-import {
-    loadFixture
-} from '@nomicfoundation/hardhat-toolbox-viem/network-helpers';
-import { expect } from 'chai';
-import { deployDownstream } from '../helpers/parthelper';
-import { ActionArgTypeEnumVal } from '../../../cli/src/utils/manifest';
+import { loadFixture } from "@nomicfoundation/hardhat-toolbox-viem/network-helpers";
+import { expect } from "chai";
+import { deployDownstream } from "../helpers/parthelper";
+import { ActionArgTypeEnumVal } from "../../../cli/src/utils/manifest";
 
-
-it("should set a numeric state variable on click from action trigger arg", async function() {
+it("should set a numeric state variable on click from action trigger arg", async function () {
     const { newPartKind, walletClient } = await loadFixture(deployDownstream);
 
     const ButtonKind = await newPartKind({
-        name: 'Button',
-        model: 'clicky-button',
-        actions: [
-            { name: 'click', args: [{name: 'newVal', type: 'int64'}] }
-        ],
-        state: [
-            { name: 'clicked', type: 'uint64' },
-        ],
+        name: "Button",
+        model: "clicky-button",
+        actions: [{ name: "click", args: [{ name: "newVal", type: "int64" }] }],
+        state: [{ name: "clicked", type: "uint64" }],
         logic: [
             {
-                when: { kind: 'action', name: 'click' },
+                when: { kind: "action", name: "click" },
                 do: [
                     {
-                        kind: 'setstate',
-                        name: 'clicked',
-                        value: { kind: 'trigger', name: 'newVal' }
-                    }
-                ]
-            }
-        ]
+                        kind: "setstate",
+                        name: "clicked",
+                        value: { kind: "trigger", name: "newVal" },
+                    },
+                ],
+            },
+        ],
     });
 
     const button = await ButtonKind.spawn([2, -2, 0]);
 
-    const valueBefore = await button.getState('clicked');
-    await button.call('click', {newVal: -5});
-    const valueAfter = await button.getState('clicked');
+    const valueBefore = await button.getState("clicked");
+    await button.call("click", { newVal: -5 });
+    const valueAfter = await button.getState("clicked");
 
     expect(valueBefore).to.equal(0);
     expect(valueAfter).to.equal(-5);
 });
 
-it("should increment count by step on click", async function() {
+it("should increment count by step on click", async function () {
     const { newPartKind, walletClient } = await loadFixture(deployDownstream);
 
     const ButtonKind = await newPartKind({
-        name: 'Button',
-        model: 'clicky-button',
-        actions: [
-            { name: 'click', args: [] }
-        ],
-        state: [
-            { name: 'clicked', type: 'uint64' },
-        ],
+        name: "Button",
+        model: "clicky-button",
+        actions: [{ name: "click", args: [] }],
+        state: [{ name: "clicked", type: "int64" }],
         logic: [
             {
-                when: { kind: 'action', name: 'click' },
+                when: { kind: "action", name: "click" },
                 do: [
                     {
-                        kind: 'incstate',
-                        name: 'clicked',
-                        step: 3
-                    }
-                ]
-            }
-        ]
+                        kind: "incstate",
+                        name: "clicked",
+                        step: {
+                            kind: "literal",
+                            type: "int64",
+                            value: "3",
+                        },
+                    },
+                ],
+            },
+        ],
     });
 
     const button = await ButtonKind.spawn([2, -2, 0]);
 
-    const countBefore = await button.getState('clicked');
-    await button.call('click', {player: walletClient.account.address});
-    const countAfter = await button.getState('clicked');
+    const countBefore = await button.getState("clicked");
+    await button.call("click", { player: walletClient.account.address });
+    const countAfter = await button.getState("clicked");
 
     expect(countBefore).to.equal(0);
     expect(countAfter).to.equal(countBefore + 3);
 });
 
-
-it("should decrement count by step on click", async function() {
+it("should decrement count by step on click", async function () {
     const { newPartKind, walletClient } = await loadFixture(deployDownstream);
 
     const ButtonKind = await newPartKind({
-        name: 'Button',
-        model: 'clicky-button',
-        actions: [
-            { name: 'click', args: [] }
-        ],
-        state: [
-            { name: 'clicked', type: 'uint64' },
-        ],
+        name: "Button",
+        model: "clicky-button",
+        actions: [{ name: "click", args: [] }],
+        state: [{ name: "clicked", type: "int64" }],
         logic: [
             {
-                when: { kind: 'action', name: 'click' },
+                when: { kind: "action", name: "click" },
                 do: [
                     {
-                        kind: 'decstate',
-                        name: 'clicked',
-                        step: 4
-                    }
-                ]
-            }
-        ]
+                        kind: "decstate",
+                        name: "clicked",
+                        step: {
+                            kind: "literal",
+                            type: "int64",
+                            value: "4",
+                        },
+                    },
+                ],
+            },
+        ],
     });
 
     const button = await ButtonKind.spawn([2, -2, 0]);
 
-    const countBefore = await button.getState('clicked');
-    await button.call('click', {player: walletClient.account.address});
-    const countAfter = await button.getState('clicked');
+    const countBefore = await button.getState("clicked");
+    await button.call("click", { player: walletClient.account.address });
+    const countAfter = await button.getState("clicked");
 
     expect(countBefore).to.equal(0);
     expect(countAfter).to.equal(countBefore - 4);
