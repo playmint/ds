48bd07712719e43e0b46ad8eaff3b872f769a1ca
Steedie <Steediekid@gmail.com>
2024-02-12 19:03:13 +0000

    WIP - trying to make sure tile is empty

--- cli/src/commands/test.ts
+++ cli/src/commands/test.ts
@@ -1,11 +1,12 @@
 import { ethers } from 'ethers';
 import { pipe, take, toPromise } from 'wonka';
-import { CompoundKeyEncoder, NodeSelectors, getCoords } from "@downstream/core";
+import { CompoundKeyEncoder, NodeSelectors, getCoords, WorldTileFragment } from "@downstream/core";
+import { getBuildingAtTile } from '@downstream/core/src/utils';
 import util from 'node:util';
 import { spawn } from 'node:child_process';
+import { getWorld } from './get';
 
 const spawnAsync = util.promisify(spawn);
-let buildings: string[];
 
 const concurrency = {
     command: 'concurrency',
@@ -25,10 +26,7 @@ const concurrency = {
     ,
     handler: async (ctx) => {
         const maxConnections: number = ctx.maxConnections || 1;
-        buildings = ctx.buildings ? ctx.buildings.split(",") : [];
-        buildings.forEach(building => {
-            console.log(`building added to array: ${building}`)
-        });
+        const buildings = ctx.buildings ? ctx.buildings : undefined;
 
         // generate wallets
         const wallets: ethers.HDNodeWallet[] = [];
@@ -43,6 +41,7 @@ const concurrency = {
             procs.push(spawnAsync('ds', [
                 '-n', `${ctx.network || 'local'}`,
                 `-k`, `${wallet.privateKey}`,
+                `-b`, `${buildings ? buildings : '0x0'}`,
                 `test`, `chaos-unit`
             ], {stdio: ['ignore', process.stdout, process.stderr]}));
         }
@@ -55,6 +54,14 @@ const concurrency = {
 const chaosUnit = {
     command: 'chaos-unit',
     describe: 'spawn a unit and keep moving it',
+    builder: (yargs) =>
+        yargs
+            .option('buildings', {
+                alias: 'b',
+                describe: 'list of BuildingKind ids to randomly place to randomly place',
+                type: 'string',
+            })
+    ,
     handler: async (ctx) => {
         const player = await ctx.player();
 
@@ -67,6 +74,7 @@ const chaosUnit = {
         const client = await ctx.client();
         const res: any = await pipe(client.query(GET_TILES, { gameID: ctx.game }, {subscribe: false}), take(1), toPromise);
         const validCoords = res.game.state.tiles.map(getCoords);
+        const buildings:string[] = ctx.buildings ? ctx.buildings.split(",") : [];
 
         // move unit around randomly
         let [q,r,s] = [0,0,0];
@@ -82,11 +90,32 @@ const chaosUnit = {
             // place building
             if (buildings?.length > 0){
                 let selectedBuilding = buildings[Math.floor(Math.random() * buildings.length)];
-                console.log(`I want to place: ${selectedBuilding}`);
+                    // WORK IN PROGRESS...
+                    // Trying to make sure there's not already a buildin on the tile
+                // console.log(`I want to place: ${selectedBuilding}`);
+                //const world = await getWorld(ctx);
+                // const coords = world.buildings[Math.floor(Math.random() * world.buildings.length)]?.location?.tile?.coords // random building in world
+                //const coords = world.buildings[5]?.location?.tile?.coords;
+
+                const tileMap = new Map<string, PassableTile>();
+                const t = tileMap.get(`${q}:${r}:${s}`);
+                console.log(t);
+                
+                //getBuildingAtTile(world.buildings, t.id);
+                //console.log(coords);
+                await player.dispatch({ name: 'DEV_SPAWN_BUILDING', args: [selectedBuilding, q, r, s] }).then(res => res.wait());
             }
         }
     },
 };
+
+interface PassableTile {
+    idx: number;
+    q: number;
+    r: number;
+    s: number;
+    tile: WorldTileFragment;
+}
 
 export const test = {
     command: 'test',
