2d7a337407272a144ee30877cbde45a1021f39d5
chrisfarms <chris@farmiloe.com>
2024-02-12 22:09:43 +0000

    update ds-cli with new network urls

--- cli/src/utils/networks.ts
+++ cli/src/utils/networks.ts
@@ -6,71 +6,23 @@ export type Network = {
     httpEndpoint: string;
 };
 
-export const networks: Network[] = [
-    {
-        name: 'hexwood0',
-        description: 'ephemeral private invite only test network',
-        wsEndpoint: 'wss://services0.downstream.game/query',
-        httpEndpoint: 'https://services0.downstream.game/query',
-    },
-    {
-        name: 'hexwood1',
-        description: 'ephemeral private invite only test network',
-        wsEndpoint: 'wss://services1.downstream.game/query',
-        httpEndpoint: 'https://services1.downstream.game/query',
-    },
-    {
-        name: 'hexwood2',
-        description: 'ephemeral private invite only test network',
-        wsEndpoint: 'wss://services2.downstream.game/query',
-        httpEndpoint: 'https://services2.downstream.game/query',
-    },
-    {
-        name: 'hexwood3',
-        description: 'ephemeral private invite only test network',
-        wsEndpoint: 'wss://services3.downstream.game/query',
-        httpEndpoint: 'https://services3.downstream.game/query',
-    },
-    {
-        name: 'hexwood4',
-        description: 'ephemeral private invite only test network',
-        wsEndpoint: 'wss://services4.downstream.game/query',
-        httpEndpoint: 'https://services4.downstream.game/query',
-    },
-    {
-        name: 'hexwood5',
-        description: 'ephemeral private invite only test network',
-        wsEndpoint: 'wss://services5.downstream.game/query',
-        httpEndpoint: 'https://services5.downstream.game/query',
-    },
-    {
-        name: 'hexwood6',
-        description: 'ephemeral private invite only test network',
-        wsEndpoint: 'wss://services6.downstream.game/query',
-        httpEndpoint: 'https://services6.downstream.game/query',
-    },
-    {
-        name: 'hexwood7',
-        description: 'ephemeral private invite only test network',
-        wsEndpoint: 'wss://services7.downstream.game/query',
-        httpEndpoint: 'https://services7.downstream.game/query',
-    },
-    {
-        name: 'hexwood8',
-        description: 'ephemeral private invite only test network',
-        wsEndpoint: 'wss://services8.downstream.game/query',
-        httpEndpoint: 'https://services8.downstream.game/query',
-    },
-    {
-        name: 'hexwood9',
-        description: 'ephemeral private invite only test network',
-        wsEndpoint: 'wss://services9.downstream.game/query',
-        httpEndpoint: 'https://services9.downstream.game/query',
-    },
+const ephemeralNetworks = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16].map((i) => ({
+    name: `hexwood${i}`,
+    description: 'ephemeral private test network',
+    wsEndpoint: `wss://services-hexwood${i}.downstream.game/query`,
+    httpEndpoint: `https://services-hexwood${i}.downstream.game/query`,
+}))
+
+const localNetworks = [
     {
         name: 'local',
         description: 'ephermal local instance of the game',
         wsEndpoint: 'ws://localhost:8080/query',
         httpEndpoint: 'http://localhost:8080/query',
     },
+];
+
+export const networks: Network[] = [
+    ...ephemeralNetworks,
+    ...localNetworks,
 ];
