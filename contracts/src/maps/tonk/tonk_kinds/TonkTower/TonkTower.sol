9829f0b862bc22ed815c61fa57dc7f848ee5660c
Steedie <Steediekid@gmail.com>
2024-03-07 12:59:51 +0000

    Moved changes to croissant map

--- contracts/src/maps/tonk/tonk_kinds/TonkTower/TonkTower.sol
+++ contracts/src/maps/tonk/tonk_kinds/TonkTower/TonkTower.sol
@@ -6,7 +6,5 @@ import {Actions} from "@ds/actions/Actions.sol";
 import {BuildingKind} from "@ds/ext/BuildingKind.sol";
 
 contract TonkTower is BuildingKind {
-    function use(Game ds, bytes24 buildingInstance, bytes24, /*actor*/ bytes memory /*payload*/ ) public override {
-        ds.getDispatcher().dispatch(abi.encodeCall(Actions.CRAFT, (buildingInstance)));
-    }
+
 }
