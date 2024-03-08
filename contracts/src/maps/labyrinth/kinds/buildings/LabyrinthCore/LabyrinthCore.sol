85c47f917e04ad7e399d4c60a19c94ceedf7a949
Jimmy Whitwell <jimmy.whitwell@playmint.com>
2024-03-08 13:30:08 +0000

    Building coordinates positioned relative to the core building instead of absolute positions

--- contracts/src/maps/labyrinth/kinds/buildings/LabyrinthCore/LabyrinthCore.sol
+++ contracts/src/maps/labyrinth/kinds/buildings/LabyrinthCore/LabyrinthCore.sol
@@ -21,18 +21,36 @@ contract LabyrinthCore is BuildingKind {
         // Crafting the Playtest Pass
         ds.getDispatcher().dispatch(abi.encodeCall(Actions.CRAFT, (buildingInstance)));
 
+        // Get location of buildingInstance
+        State state = ds.getState();
+        bytes24 buildingTile = state.getFixedLocation(buildingInstance);
+        int16 coreQ = int16(int192(uint192(buildingTile) >> 32));
+        int16 coreR = int16(int192(uint192(buildingTile) >> 16));
+        int16 coreS = int16(int192(uint192(buildingTile)));
+
         // resetting the map
         // rocks at room 4
-        ds.getDispatcher().dispatch(abi.encodeCall(Actions.DEV_SPAWN_BUILDING, (_LARGE_ROCKS, 11, -10, -1)));
-        ds.getDispatcher().dispatch(abi.encodeCall(Actions.DEV_SPAWN_BUILDING, (_LARGE_ROCKS, 13, -12, -1)));
-        ds.getDispatcher().dispatch(abi.encodeCall(Actions.DEV_SPAWN_BUILDING, (_LARGE_ROCKS, 13, -14, 1)));
-        ds.getDispatcher().dispatch(abi.encodeCall(Actions.DEV_SPAWN_BUILDING, (_LARGE_ROCKS, 11, -12, 1)));
+        ds.getDispatcher().dispatch(
+            abi.encodeCall(Actions.DEV_SPAWN_BUILDING, (_LARGE_ROCKS, coreQ + 5, coreR + -4, coreS + -1))
+        ); // 11, -10, -1
+        ds.getDispatcher().dispatch(
+            abi.encodeCall(Actions.DEV_SPAWN_BUILDING, (_LARGE_ROCKS, coreQ + 7, coreR + -6, coreS + -1))
+        ); // 13, -12, -1
+        ds.getDispatcher().dispatch(
+            abi.encodeCall(Actions.DEV_SPAWN_BUILDING, (_LARGE_ROCKS, coreQ + 7, coreR + -8, coreS + 1))
+        ); // 13, -14, 1
+        ds.getDispatcher().dispatch(
+            abi.encodeCall(Actions.DEV_SPAWN_BUILDING, (_LARGE_ROCKS, coreQ + 5, coreR + -6, coreS + 1))
+        ); // 11, -12, 1
         //crusher at room 5
-        ds.getDispatcher().dispatch(abi.encodeCall(Actions.DEV_SPAWN_BUILDING, (_CRUSHER, 6, -12, 6)));
+        ds.getDispatcher().dispatch(
+            abi.encodeCall(Actions.DEV_SPAWN_BUILDING, (_CRUSHER, coreQ + 0, coreR + -6, coreS + 6))
+        ); // 6, -12, 6
+
         //items at room 3
-        int16 q = 14;
-        int16 r = -7;
-        int16 s = -7;
+        int16 q = coreQ + 8; // 14
+        int16 r = coreR + -1; // -7
+        int16 s = coreS + -7; // -7
         bytes24 bagId = bytes24(abi.encodePacked(Kind.Bag.selector, uint96(0), int16(0), q, r, s));
         bytes24 tileId = bytes24(abi.encodePacked(Kind.Tile.selector, uint96(0), int16(0), q, r, s));
         bytes24[] memory items = new bytes24[](4);
@@ -48,87 +66,5 @@ contract LabyrinthCore is BuildingKind {
         ds.getDispatcher().dispatch(
             abi.encodeCall(Actions.DEV_SPAWN_BAG, (bagId, address(0), tileId, 0, items, balances))
         );
-    }
-
-    // version of use that restricts crafting to building owner, author or allow list
-    // these restrictions will not be reflected in the UI unless you make
-    // similar changes in BasicFactory.js
-    /*function use(Game ds, bytes24 buildingInstance, bytes24 actor, bytes memory ) public override {
-        State state = GetState(ds);
-        CheckIsFriendlyUnit(state, actor, buildingInstance);
-
-        ds.getDispatcher().dispatch(abi.encodeCall(Actions.CRAFT, (buildingInstance)));
-    }*/
-
-    // version of use that restricts crafting to units carrying a certain item
-    /*function use(Game ds, bytes24 buildingInstance, bytes24 actor, bytes memory ) public override {
-        // require carrying an idCard
-        // you can change idCardItemId to another item id
-        CheckIsCarryingItem(state, actor, idCardItemId);
-    
-        ds.getDispatcher().dispatch(abi.encodeCall(Actions.CRAFT, (buildingInstance)));
-    }*/
-
-    function GetState(Game ds) internal returns (State) {
-        return ds.getState();
-    }
-
-    function GetBuildingOwner(State state, bytes24 buildingInstance) internal view returns (bytes24) {
-        return state.getOwner(buildingInstance);
-    }
-
-    function GetBuildingAuthor(State state, bytes24 buildingInstance) internal view returns (bytes24) {
-        bytes24 buildingKind = state.getBuildingKind(buildingInstance);
-        return state.getOwner(buildingKind);
-    }
-
-    function CheckIsFriendlyUnit(State state, bytes24 actor, bytes24 buildingInstance) internal view {
-        require(
-            UnitOwnsBuilding(state, actor, buildingInstance) || UnitAuthoredBuilding(state, actor, buildingInstance)
-                || UnitOwnedByFriendlyPlayer(state, actor),
-            "Unit does not have permission to use this building"
-        );
-    }
-
-    function UnitOwnsBuilding(State state, bytes24 actor, bytes24 buildingInstance) internal view returns (bool) {
-        return state.getOwner(actor) == GetBuildingOwner(state, buildingInstance);
-    }
-
-    function UnitAuthoredBuilding(State state, bytes24 actor, bytes24 buildingInstance) internal view returns (bool) {
-        return state.getOwner(actor) == GetBuildingAuthor(state, buildingInstance);
-    }
-
-    address[] private friendlyPlayerAddresses = [0x402462EefC217bf2cf4E6814395E1b61EA4c43F7];
-
-    function UnitOwnedByFriendlyPlayer(State state, bytes24 actor) internal view returns (bool) {
-        address ownerAddress = state.getOwnerAddress(actor);
-        for (uint256 i = 0; i < friendlyPlayerAddresses.length; i++) {
-            if (friendlyPlayerAddresses[i] == ownerAddress) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    // use cli command 'ds get items' for all current possible ids.
-    bytes24 idCardItemId = 0x6a7a67f0b29554460000000100000064000000640000004c;
-
-    function CheckIsCarryingItem(State state, bytes24 actor, bytes24 item) internal view {
-        require((UnitIsCarryingItem(state, actor, item)), "Unit must be carrying specified item");
-    }
-
-    function UnitIsCarryingItem(State state, bytes24 actor, bytes24 item) internal view returns (bool) {
-        for (uint8 bagIndex = 0; bagIndex < 2; bagIndex++) {
-            bytes24 bag = state.getEquipSlot(actor, bagIndex);
-            if (bag != 0) {
-                for (uint8 slot = 0; slot < 4; slot++) {
-                    (bytes24 resource, /*uint64 balance*/ ) = state.getItemSlot(bag, slot);
-                    if (resource == item) {
-                        return true;
-                    }
-                }
-            }
-        }
-        return false;
     }
 }
