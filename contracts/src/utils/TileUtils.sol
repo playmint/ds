44260d960ccf83821d65c5c26bacd39d3d6a440d
JackPlaymint <jack.burfield@playmint.com>
2024-04-09 12:35:12 +0100

    Added zone check to TileUtils

--- contracts/src/utils/TileUtils.sol
+++ contracts/src/utils/TileUtils.sol
@@ -2,11 +2,13 @@
 pragma solidity ^0.8.13;
 
 import {State, CompoundKeyEncoder, CompoundKeyDecoder} from "cog/IState.sol";
-import {Schema, Node, Kind, Q, R, S} from "@ds/schema/Schema.sol";
+import {Schema, Node, Kind, Z, Q, R, S} from "@ds/schema/Schema.sol";
 
 using Schema for State;
 
 library TileUtils {
+    uint256 constant $MAX_INT = 2**256 - 1;
+
     function coords(bytes24 tile) internal pure returns (int16[4] memory keys) {
         keys = CompoundKeyDecoder.INT16_ARRAY(tile);
     }
@@ -14,6 +16,10 @@ library TileUtils {
     function distance(bytes24 tileA, bytes24 tileB) internal pure returns (uint256) {
         int16[4] memory a = CompoundKeyDecoder.INT16_ARRAY(tileA);
         int16[4] memory b = CompoundKeyDecoder.INT16_ARRAY(tileB);
+        if(a[Z] != b[Z]) // If the two tiles are in different zones, return the maximum distance
+        {
+            return $MAX_INT;
+        }
         return uint256(
             (abs(int256(a[Q]) - int256(b[Q])) + abs(int256(a[R]) - int256(b[R])) + abs(int256(a[S]) - int256(b[S]))) / 2
         );
