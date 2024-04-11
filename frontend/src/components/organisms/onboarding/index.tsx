e80d32d777afeed5c2beed5dcb943e03d53a685a
Steedie <Steediekid@gmail.com>
2024-04-11 12:41:58 +0100

    owner welcome section & nav-panel updates

--- frontend/src/components/organisms/onboarding/index.tsx
+++ frontend/src/components/organisms/onboarding/index.tsx
@@ -1,4 +1,5 @@
 import { ConnectedPlayer, WorldMobileUnitFragment, ZoneWithBags } from '@app/../../core/src';
+import { useWallet } from '@app/hooks/use-game-state';
 import { StyledHeaderPanel } from '@app/styles/base-panel.styles';
 import { ActionButton } from '@app/styles/button.styles';
 import { useCallback, useState } from 'react';
@@ -53,17 +54,47 @@ export const Onboarding = ({ player, playerUnits, onClickConnect, zone, block }:
         (u) => u.nextLocation && u.nextLocation.time + ACTIVE_UNIT_TIMEOUT < block
     );
 
+    const { wallet } = useWallet();
+    const address = player?.addr || wallet?.address || '';
+    const isZoneOwner = address === zone?.owner?.addr;
+
     return (
         <StyledOnboarding>
             <div className="header">
-                <h3>👁️‍🗨️ {zoneName}</h3>
+                <h3>{zoneName}</h3>
+                <div className="description">
+                    <p>{zoneDescription}</p>
+                </div>
             </div>
+
             <div className="content">
                 <p></p>
-                <p>
-                    welcome to {zoneName}, {zoneDescription}
-                </p>
+                {isZoneOwner && (
+                    <fieldset>
+                        <legend>Owner Information</legend>
+                        <p>Welcome to your island!</p> <br />
+                        <p>Here are some links you might find useful:</p>
+                        <a
+                            href="https://github.com/playmint/ds/tree/main/tutorial#readme"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            Builder Tutorials
+                        </a>
+                        <br />
+                        <a href="/tile-fabricator" target="_blank" rel="noopener noreferrer">
+                            Tile Fabricator
+                        </a>
+                        <br />
+                        <a href="/building-fabricator" target="_blank" rel="noopener noreferrer">
+                            Building Fabricator
+                        </a>
+                        <br />
+                    </fieldset>
+                )}
+                <br />
                 <p>There are {activeUnits.length} active units here</p>
+                <br />
                 {player && playerUnits.length === 0 ? (
                     <ActionButton onClick={spawnMobileUnit} disabled={isSpawningMobileUnit}>
                         Enter
