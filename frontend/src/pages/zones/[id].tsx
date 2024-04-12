6098bc7d91387c7de6dc965c7aa0834d1a019bc5
chrisfarms <chris@farmiloe.com>
2024-04-12 13:08:07 +0100

    generate all 32k zone pages ... yep
    since we statically export to html, we need all the zone pages to exist,
    so generate 32k of them (which is the max number of zones).
    
    ideally there would just be one page that we rewrite to, but that would
    require us to not do the export version of nextjs app so we are goign
    the brute force way for now

--- frontend/src/pages/zones/[id].tsx
+++ frontend/src/pages/zones/[id].tsx
@@ -7,12 +7,16 @@ import { UnityMapProvider } from '@app/hooks/use-unity-map';
 import { WalletProviderProvider } from '@app/hooks/use-wallet-provider';
 import { InventoryProvider } from '@app/plugins/inventory/inventory-provider';
 import { CompoundKeyEncoder, NodeSelectors } from '@downstream/core';
-import { useRouter } from 'next/router';
+// import { useRouter } from 'next/router';
 
-export default function ZonePage() {
+export type ZonePageProps = {
+    id: string;
+};
+
+export default function ZonePage({ id }: ZonePageProps) {
     const config = useConfig();
-    const router = useRouter();
-    const zoneParam = Array.isArray(router.query.id) ? router.query.id[0] : router.query.id;
+    // const router = useRouter();
+    const zoneParam = id; //Array.isArray(router.query.id) ? router.query.id[0] : router.query.id;
     const zoneId = zoneParam ? CompoundKeyEncoder.encodeUint160(NodeSelectors.Zone, BigInt(zoneParam)) : undefined;
     if (!zoneId) {
         return <div>INVALID_ZONE_ID</div>;
@@ -32,4 +36,16 @@ export default function ZonePage() {
             </WalletProviderProvider>
         </UnityMapProvider>
     );
+}
+
+export async function getStaticPaths() {
+    const paths = Array.from(new Array(32000)).map((_, idx) => ({
+        params: { id: `${idx + 1}` },
+    }));
+    return { paths, fallback: false };
+}
+
+export async function getStaticProps({ params }) {
+    const props: ZonePageProps = { id: params.id };
+    return { props };
 }
