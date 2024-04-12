99026bc584c9919aab901ecb40aaac5f7e771a51
chrisfarms <chris@farmiloe.com>
2024-04-12 14:26:36 +0100

    unused var

--- contracts/src/rules/NewPlayerRule.sol
+++ contracts/src/rules/NewPlayerRule.sol
@@ -27,12 +27,8 @@ contract NewPlayerRule is Rule {
             state.setPrevLocation(mobileUnit, locationTile, 0);
             state.setNextLocation(mobileUnit, locationTile, ctx.clock);
             // give the mobileUnit a couple of bags
-            bytes24 bag0 = _spawnBag(state, mobileUnit, ctx.sender, 0);
+            _spawnBag(state, mobileUnit, ctx.sender, 0);
             bytes24 bag1 = _spawnBag(state, mobileUnit, ctx.sender, 1);
-
-            //give the mobileUnit the two starter items
-            //state.setItemSlot(bag0, 0, ItemUtils.AcceptanceLetter(), 1);
-            //state.setItemSlot(bag0, 1, ItemUtils.IDCard(), 1);
 
             //give the mobileUnit 100 of each of the base materials
             state.setItemSlot(bag1, 0, ItemUtils.GreenGoo(), 100);
