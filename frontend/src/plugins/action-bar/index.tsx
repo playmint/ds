e16fe95331ddad67041f3c4ac35d0a688a642482
chrisfarms <chris@farmiloe.com>
2024-05-11 17:14:59 +0100

    disable construct intent for buggy zones
    there's a contract bug preventing the CONSTRUCT_BUILDING_MOBILE_UNIT
    action for working as intended when the zone id is 40 or more.
    
    as a temporary measure we are disabling the build intent for the
    affected zones so that it reduces the potential confusion of either the
    resulting "out of bounds" error or the silent failing due to these
    errors only being debug visisble.

--- frontend/src/plugins/action-bar/index.tsx
+++ frontend/src/plugins/action-bar/index.tsx
@@ -11,13 +11,17 @@ const CONSTRUCT_INTENT = 'construct';
 const MOVE_INTENT = 'move';
 const COMBAT_INTENT = 'combat';
 
-export interface ActionBarProps extends ComponentProps {}
+export interface ActionBarProps extends ComponentProps {
+    construct: boolean;
+    move: boolean;
+    combat: boolean;
+}
 
 const StyledActionBar = styled('div')`
     ${styles}
 `;
 
-export const ActionBar: FunctionComponent<ActionBarProps> = ({}: ActionBarProps) => {
+export const ActionBar: FunctionComponent<ActionBarProps> = ({ construct, move, combat }: ActionBarProps) => {
     const { selectIntent, intent, mobileUnit, selectTiles, selectMapElement } = useSelection();
 
     const handleSelectIntent = useCallback(
@@ -44,24 +48,30 @@ export const ActionBar: FunctionComponent<ActionBarProps> = ({}: ActionBarProps)
     return (
         <StyledActionBar>
             <div className="actions">
-                <UnitActionButton
-                    className={`${intent === MOVE_INTENT ? 'toggleOn' : ''}`}
-                    onClick={() => handleSelectIntent(MOVE_INTENT)}
-                >
-                    Move
-                </UnitActionButton>
-                <UnitActionButton
-                    className={`${intent === CONSTRUCT_INTENT ? 'toggleOn' : ''}`}
-                    onClick={() => handleSelectIntent(CONSTRUCT_INTENT)}
-                >
-                    Build
-                </UnitActionButton>
-                <UnitActionButton
-                    className={`${intent === COMBAT_INTENT ? 'toggleOn' : ''}`}
-                    onClick={() => handleSelectIntent(COMBAT_INTENT)}
-                >
-                    Attack
-                </UnitActionButton>
+                {move && (
+                    <UnitActionButton
+                        className={`${intent === MOVE_INTENT ? 'toggleOn' : ''}`}
+                        onClick={() => handleSelectIntent(MOVE_INTENT)}
+                    >
+                        Move
+                    </UnitActionButton>
+                )}
+                {construct && (
+                    <UnitActionButton
+                        className={`${intent === CONSTRUCT_INTENT ? 'toggleOn' : ''}`}
+                        onClick={() => handleSelectIntent(CONSTRUCT_INTENT)}
+                    >
+                        Build
+                    </UnitActionButton>
+                )}
+                {combat && (
+                    <UnitActionButton
+                        className={`${intent === COMBAT_INTENT ? 'toggleOn' : ''}`}
+                        onClick={() => handleSelectIntent(COMBAT_INTENT)}
+                    >
+                        Attack
+                    </UnitActionButton>
+                )}
             </div>
         </StyledActionBar>
     );
